34015ef27c59189d3c1191440c245188dc90741d
Viktor Stanchev <me@viktorstanchev.com>
2015-04-18 15:16:15 -0400

    make sudo rule work with pacman

--- thefuck/rules/sudo.py
+++ thefuck/rules/sudo.py
@@ -1,6 +1,7 @@
 def match(command, settings):
     return ('permission denied' in command.stderr.lower()
             or 'EACCES' in command.stderr
+            or 'error: you cannot perform this operation unless you are root.' in command.stderr
             or 'pkg: Insufficient privileges' in command.stderr)
 
 
