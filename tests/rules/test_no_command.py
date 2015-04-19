ab91eaf73d2012266aaa3f82d08d05e62a82c4a4
nvbn <nvbn.rm@gmail.com>
2015-04-19 14:58:44 +0200

    Revert "Merge pull request #36 from jjmason/improve-no-command"
    This reverts commit 28a5c1214b730e727b88648cf56dd36d51862ff6, reversing
    changes made to 3f0cbb9326895cf516eb9f4f0f834d23200dc21e.

--- tests/rules/test_no_command.py
+++ tests/rules/test_no_command.py
@@ -9,19 +9,7 @@ from thefuck.main import Command
 def command_found():
     return b'''No command 'aptget' found, did you mean:
  Command 'apt-get' from package 'apt' (main)
- Command 'not-installed' from package 'derp' (main)
- Command 'not-really-used' from package 'whatever' (main)
 aptget: command not found
-'''
-
-@pytest.fixture
-def uninstalled_command_found():
-    return b'''No command 'pish' found, did you mean:
- Command 'vish' from package 'vish' (universe)
- Command 'wish' from package 'tk' (main)
- Command 'fish' from package 'fish' (universe)
- Command 'pdsh' from package 'pdsh' (universe)
-pish: command not found
 '''
 
 @pytest.fixture
@@ -30,32 +18,13 @@ def command_not_found():
 vom: command not found
 '''
 
+
 @pytest.fixture
 def bins_exists(request):
     p = patch('thefuck.rules.no_command.which',
               return_value=True)
     p.start()
     request.addfinalizer(p.stop)
-
-@pytest.fixture
-def bin_might_exist(request):
-    def side_effect(name):
-        return name in ['not-really-used', 'apt-get', '/usr/lib/command-not-found', 'test']
-    p = patch('thefuck.rules.no_command.which',
-              side_effect = side_effect)
-    p.start()
-    request.addfinalizer(p.stop)
-            
-
-@pytest.fixture
-def patch_history(request):
-    def side_effect(name):
-        return 2 if name == 'not-really-used' else 12
-    p = patch('thefuck.rules.no_command._count_history_uses',
-              side_effect = side_effect)
-    p.start()
-    request.addfinalizer(p.stop)
-    
 
 
 @pytest.fixture
@@ -65,12 +34,12 @@ def settings():
     return _Settings
 
 
-@pytest.mark.usefixtures('bin_might_exist', 'patch_history')
-def test_match(command_found, command_not_found, uninstalled_command_found, settings):
+@pytest.mark.usefixtures('bins_exists')
+def test_match(command_found, command_not_found, settings):
     with patch('thefuck.rules.no_command.Popen') as Popen:
         Popen.return_value.stderr.read.return_value = command_found
         assert match(Command('aptget install vim', '', ''), settings)
-        Popen.assert_called_with('/usr/lib/command-not-found aptget',
+        Popen.assert_called_once_with('/usr/lib/command-not-found aptget',
                                       shell=True, stderr=PIPE)
         Popen.return_value.stderr.read.return_value = command_not_found
         assert not match(Command('ls', '', ''), settings)
@@ -79,14 +48,11 @@ def test_match(command_found, command_not_found, uninstalled_command_found, sett
         Popen.return_value.stderr.read.return_value = command_found
         assert match(Command('sudo aptget install vim', '', ''),
                      Mock(command_not_found='test'))
-        Popen.assert_called_with('test aptget',
+        Popen.assert_called_once_with('test aptget',
                                       shell=True, stderr=PIPE)
 
-    with patch('thefuck.rules.no_command.Popen') as Popen:
-        Popen.return_value.stderr.read.return_value = uninstalled_command_found
-        assert not match(Command('pish bla blah', '', ''), settings)
 
-@pytest.mark.usefixtures('bin_might_exist', 'patch_history')
+@pytest.mark.usefixtures('bins_exists')
 def test_get_new_command(command_found):
     with patch('thefuck.rules.no_command._get_output',
                return_value=command_found.decode()):
