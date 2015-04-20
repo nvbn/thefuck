2db0a215b4006555b21281957fe8e8446da1beb7
nvbn <nvbn.rm@gmail.com>
2015-04-20 21:48:54 +0200

    Fix fuck when more than one git command available

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='thefuck',
-      version=1.13,
+      version=1.14,
       description="Magnificent app which corrects your previous console command",
       author='Vladimir Iakovlev',
       author_email='nvbn.rm@gmail.com',
--- tests/rules/test_git_not_command.py
+++ tests/rules/test_git_not_command.py
@@ -13,16 +13,32 @@ branch
 
 
 @pytest.fixture
+def git_not_command_one_of_this():
+    return """git: 'st' is not a git command. See 'git --help'.
+
+Did you mean one of these?
+status
+reset
+stage
+stash
+stats
+"""
+
+
+@pytest.fixture
 def git_command():
     return "* master"
 
 
-def test_match(git_not_command, git_command):
+def test_match(git_not_command, git_command, git_not_command_one_of_this):
     assert match(Command('git brnch', '', git_not_command), None)
+    assert match(Command('git st', '', git_not_command_one_of_this), None)
     assert not match(Command('ls brnch', '', git_not_command), None)
     assert not match(Command('git branch', '', git_command), None)
 
 
-def test_get_new_command(git_not_command):
+def test_get_new_command(git_not_command, git_not_command_one_of_this):
     assert get_new_command(Command('git brnch', '', git_not_command), None)\
         == 'git branch'
+    assert get_new_command(
+        Command('git st', '', git_not_command_one_of_this), None) == 'git status'
