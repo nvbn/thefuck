e1fe7ff7d02abe9fc1a447bf49eb97b37ca7b18d
nvbn <nvbn.rm@gmail.com>
2015-04-21 06:56:26 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='thefuck',
-      version=1.17,
+      version=1.18,
       description="Magnificent app which corrects your previous console command",
       author='Vladimir Iakovlev',
       author_email='nvbn.rm@gmail.com',
