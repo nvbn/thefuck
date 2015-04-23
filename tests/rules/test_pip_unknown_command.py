af2bfe7c582c588db7f2a148d46e1cf588938165
Namwoo Kim <iam@kimtree.net>
2015-04-23 15:25:12 +0900

    Add a support for pip unknown commands

---
+++ tests/rules/test_pip_unknown_command.py
@@ -0,0 +1,24 @@
+import pytest
+from thefuck.types import Command
+from thefuck.rules.pip_unknown_command import match, get_new_command
+
+
+@pytest.fixture
+def pip_unknown_cmd():
+    return '''ERROR: unknown command "instatl" - maybe you meant "install"'''
+
+
+@pytest.fixture
+def pip_unknown_cmd_without_recommend():
+    return '''ERROR: unknown command "i"'''
+
+
+def test_match(pip_unknown_cmd, pip_unknown_cmd_without_recommend):
+    assert match(Command('pip instatl', '', pip_unknown_cmd), None)
+    assert not match(Command('pip i', '', pip_unknown_cmd_without_recommend),
+                     None)
+
+
+def test_get_new_command(pip_unknown_cmd):
+    assert get_new_command(Command('pip instatl', '', pip_unknown_cmd), None)\
+        == 'pip install'
