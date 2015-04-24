a454438939e8c021a37ebe081d0b2dd4fef39667
Joseph Henrich <crimsonknave@gmail.com>
2015-04-23 20:28:19 -0400

    Fuck, I wanted ls -lah

---
+++ thefuck/rules/ls_lah.py
@@ -0,0 +1,7 @@
+def match(command, settings):
+    return ('ls' in command.script and not ('ls -' in command.script))
+
+def get_new_command(command, settings):
+    command = command.script.split(' ')
+    command[0] = 'ls -lah'
+    return ' '.join(command)
