8a49b40f6a7dea32f72ccf733eca7b6e84c03686
SanketDG <sanketdasgupta@gmail.com>
2015-05-01 00:12:43 +0530

    add entry point

--- setup.py
+++ setup.py
@@ -17,4 +17,4 @@ setup(name='thefuck',
       zip_safe=False,
       install_requires=['pathlib', 'psutil', 'colorama', 'six'],
       entry_points={'console_scripts': [
-          'thefuck = thefuck.main:main']})
+          'thefuck = thefuck.main:main', 'thefuck-alias = thefuck.main:alias']})
