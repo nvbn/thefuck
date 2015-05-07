db3b5f5e5fcbb441fb047335f357504fef50d39f
Jared Henry Oviatt <jaredhenryoviatt@gmail.com>
2015-05-06 18:05:43 -0700

    started learning abilities

---
+++ thefuck/rules/my_fuckups.py
@@ -0,0 +1,15 @@
+
+import custom_fuckups
+
+fuckups = custom_fuckups.get_fuckups()
+
+def match(command, settings):
+  if command in fuckups:
+    return True
+  return False # not found
+
+
+def get_new_command(command, settings):
+  return fuckups[command]
+
+# priority = 1000  # Lower first
