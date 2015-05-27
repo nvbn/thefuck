a9e3b22fa45f0da1221ddcbea3a2f62b8dbba3bf
Cami Diez <diezcami@gmail.com>
2015-05-27 15:38:46 +0800

    Added javac rule

---
+++ thefuck/rules/javac.py
@@ -0,0 +1,15 @@
+# Appends .java when compiling java files
+# 
+# Example:
+# > javac foo
+# error: Class names, 'foo', are only accepted if annotation 
+# processing is explicitly requested
+#
+# 
+
+def match(command, settings):
+	return (command.script.startswith ('javac ')
+			and not command.script.endswith('.java'))
+
+def get_new_command(command, settings):
+	return command.script + '.java'
