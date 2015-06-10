96fe1e77b3cfacb5e2bb8e8568a5a515f3870ebf
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-06-10 20:53:35 -0300

    refact(rules.no_command): do not add TF_ALIAS to the “callables” list
    Fix #234, #245 and #251
    
    Ref #221

--- thefuck/rules/no_command.py
+++ thefuck/rules/no_command.py
@@ -2,7 +2,7 @@ from difflib import get_close_matches
 import os
 from pathlib import Path
 from thefuck.utils import sudo_support
-from thefuck.shells import get_aliases
+from thefuck.shells import thefuck_alias, get_aliases
 
 
 def _safe(fn, fallback):
@@ -13,10 +13,12 @@ def _safe(fn, fallback):
 
 
 def _get_all_callables():
+    tf_alias = thefuck_alias()
     return [exe.name
             for path in os.environ.get('PATH', '').split(':')
             for exe in _safe(lambda: list(Path(path).iterdir()), [])
-            if not _safe(exe.is_dir, True)] + get_aliases()
+            if not _safe(exe.is_dir, True)] + [
+                alias for alias in get_aliases() if alias != tf_alias]
 
 
 @sudo_support
