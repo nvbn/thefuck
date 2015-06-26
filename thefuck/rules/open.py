01ce65047a1cd2e21b36fa0b2cee0b7be676ebe3
mcarton <cartonmartin+git@gmail.com>
2015-06-26 11:27:04 +0200

    Use spaces instead of tabs
    This is more common in python and follows other rules usage.

--- thefuck/rules/open.py
+++ thefuck/rules/open.py
@@ -1,24 +1,23 @@
 # Opens URL's in the default web browser
-# 
+#
 # Example:
 # > open github.com
 # The file ~/github.com does not exist.
 # Perhaps you meant 'http://github.com'?
 #
-# 
+
 
 def match(command, settings):
-	return (command.script.startswith ('open')
-			and (
-			# Wanted to use this:
-			# 'http' in command.stderr
-			'.com' in command.script
-			or '.net' in command.script
-			or '.org' in command.script
-			or '.ly' in command.script
-			or '.io' in command.script
-			or '.se' in command.script
-			or '.edu' in command.script))
+    return (command.script.startswith('open')
+            and (
+                '.com' in command.script
+                or '.net' in command.script
+                or '.org' in command.script
+                or '.ly' in command.script
+                or '.io' in command.script
+                or '.se' in command.script
+                or '.edu' in command.script))
+
 
 def get_new_command(command, settings):
-	return 'open http://' + command.script[5:]
+    return 'open http://' + command.script[5:]
