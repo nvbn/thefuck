3d842fe6eb4c386c7d51ca9494270aa8f948da15
nvbn <nvbn.rm@gmail.com>
2015-07-07 16:39:21 +0300

    #N/A Fix setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,19 @@ from setuptools import setup, find_packages
 import sys
 
 if sys.version_info < (2, 7):
-    print(
-      'thefuck requires Python version 2.7 or later' +
-      ' ({}.{} detected).'.format(*sys.version_info[:2]))
+    print('thefuck requires Python version 2.7 or later' +
+          ' ({}.{} detected).'.format(*sys.version_info[:2]))
     sys.exit(-1)
-
-if sys.version_info < (3, 3):
-    print(
-      'thefuck requires Python version 3.3 or later' +
-      ' ({}.{} detected).'.format(*sys.version_info[:2]))
+elif (3, 0) < sys.version_info < (3, 3):
+    print('thefuck requires Python version 3.3 or later' +
+          ' ({}.{} detected).'.format(*sys.version_info[:2]))
     sys.exit(-1)
 
 VERSION = '1.46'
 
 install_requires = ['psutil', 'colorama', 'six']
 
-if sys.version_info < (3,4):
+if sys.version_info < (3, 4):
     install_requires.append('pathlib')
 
 setup(name='thefuck',
