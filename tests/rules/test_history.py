ee87d1c5476f5d831c4304d8e2ab79340eb44fc8
nvbn <nvbn.rm@gmail.com>
2015-07-20 01:53:32 +0300

    #N/A Ignore history lines before `fuck` call in history rule

--- tests/rules/test_history.py
+++ tests/rules/test_history.py
@@ -6,7 +6,14 @@ from tests.utils import Command
 @pytest.fixture
 def history(mocker):
     return mocker.patch('thefuck.rules.history.get_history',
-                        return_value=['ls cat', 'diff x', 'nocommand x'])
+                        return_value=['le cat', 'fuck', 'ls cat',
+                                      'diff x', 'nocommand x'])
+
+
+@pytest.fixture
+def alias(mocker):
+    return mocker.patch('thefuck.rules.history.thefuck_alias',
+                        return_value='fuck')
 
 
 @pytest.fixture
@@ -15,19 +22,19 @@ def callables(mocker):
                         return_value=['diff', 'ls'])
 
 
-@pytest.mark.usefixtures('history', 'callables', 'no_memoize')
+@pytest.mark.usefixtures('history', 'callables', 'no_memoize', 'alias')
 @pytest.mark.parametrize('script', ['ls cet', 'daff x'])
 def test_match(script):
     assert match(Command(script=script), None)
 
 
-@pytest.mark.usefixtures('history', 'callables', 'no_memoize')
+@pytest.mark.usefixtures('history', 'callables', 'no_memoize', 'alias')
 @pytest.mark.parametrize('script', ['apt-get', 'nocommand y'])
 def test_not_match(script):
     assert not match(Command(script=script), None)
 
 
-@pytest.mark.usefixtures('history', 'callables', 'no_memoize')
+@pytest.mark.usefixtures('history', 'callables', 'no_memoize', 'alias')
 @pytest.mark.parametrize('script, result', [
     ('ls cet', 'ls cat'),
     ('daff x', 'diff x')])
--- thefuck/rules/history.py
+++ thefuck/rules/history.py
@@ -1,16 +1,27 @@
 from difflib import get_close_matches
-from thefuck.shells import get_history
+from thefuck.shells import get_history, thefuck_alias
 from thefuck.utils import get_closest, memoize
 from thefuck.rules.no_command import get_all_callables
 
 
+def _not_corrected(history, tf_alias):
+    """Returns all lines from history except that comes before `fuck`."""
+    previous = None
+    for line in history:
+        if previous is not None and line != tf_alias:
+            yield previous
+        previous = line
+    yield history[-1]
+
+
 @memoize
 def _history_of_exists_without_current(command):
+    history = get_history()
+    tf_alias = thefuck_alias()
     callables = get_all_callables()
-    return [line for line in get_history()
-            if line != command.script
+    return [line for line in _not_corrected(history, tf_alias)
+            if not line.startswith(tf_alias) and not line == command.script
             and line.split(' ')[0] in callables]
-
 
 def match(command, settings):
     return len(get_close_matches(command.script,
@@ -20,5 +31,6 @@ def match(command, settings):
 def get_new_command(command, settings):
     return get_closest(command.script,
                        _history_of_exists_without_current(command))
+
 
 priority = 9999
