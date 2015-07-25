129d67f79420048a21a0236ff077417724507298
nvbn <nvbn.rm@gmail.com>
2015-07-25 03:30:11 +0300

    Temporary disable functional tests with fish in travis-ci
    https://github.com/travis-ci/apt-source-whitelist/issues/71

--- tests/functional/test_fish.py
+++ tests/functional/test_fish.py
@@ -1,7 +1,7 @@
 import pytest
 from tests.functional.plots import with_confirmation, without_confirmation, \
     refuse_with_confirmation
-from tests.functional.utils import spawn, functional, images
+from tests.functional.utils import spawn, functional, images, bare
 
 containers = images(('ubuntu-python3-fish', u'''
 FROM ubuntu:latest
@@ -19,6 +19,8 @@ RUN pip2 install -U pip setuptools
 
 
 @functional
+@pytest.mark.skipif(
+    bare, 'https://github.com/travis-ci/apt-source-whitelist/issues/71')
 @pytest.mark.parametrize('tag, dockerfile', containers)
 def test_with_confirmation(tag, dockerfile):
     with spawn(tag, dockerfile, u'fish') as proc:
@@ -28,6 +30,8 @@ def test_with_confirmation(tag, dockerfile):
 
 
 @functional
+@pytest.mark.skipif(
+    bare, 'https://github.com/travis-ci/apt-source-whitelist/issues/71')
 @pytest.mark.parametrize('tag, dockerfile', containers)
 def test_refuse_with_confirmation(tag, dockerfile):
     with spawn(tag, dockerfile, u'fish') as proc:
@@ -37,6 +41,8 @@ def test_refuse_with_confirmation(tag, dockerfile):
 
 
 @functional
+@pytest.mark.skipif(
+    bare, 'https://github.com/travis-ci/apt-source-whitelist/issues/71')
 @pytest.mark.parametrize('tag, dockerfile', containers)
 def test_without_confirmation(tag, dockerfile):
     with spawn(tag, dockerfile, u'fish') as proc:
