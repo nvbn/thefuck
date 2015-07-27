af40ad84d8522dfd43c5ca1959cfd79c893e90cf
nvbn <nvbn.rm@gmail.com>
2015-07-27 22:23:26 +0300

    Bump to 2.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ elif (3, 0) < version < (3, 3):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '2.4'
+VERSION = '2.5'
 
 install_requires = ['psutil', 'colorama', 'six']
 extras_require = {':python_version<"3.4"': ['pathlib']}
