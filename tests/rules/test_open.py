1becd92b126a368d6e7d93aa8eea209414ce4aa2
mcarton <cartonmartin+git@gmail.com>
2015-08-17 16:22:05 +0200

    Fix the open rule
    It was simply wrong with `xdg-`, `gnome-` and `kde-open`.

--- tests/rules/test_open.py
+++ tests/rules/test_open.py
@@ -9,7 +9,10 @@ from tests.utils import Command
     Command(script='open foo.org'),
     Command(script='open foo.net'),
     Command(script='open foo.se'),
-    Command(script='open foo.io')])
+    Command(script='open foo.io'),
+    Command(script='xdg-open foo.com'),
+    Command(script='gnome-open foo.com'),
+    Command(script='kde-open foo.com')])
 def test_match(command):
     assert match(command, None)
 
@@ -20,6 +23,9 @@ def test_match(command):
     (Command('open foo.org'), 'open http://foo.org'),
     (Command('open foo.net'), 'open http://foo.net'),
     (Command('open foo.se'), 'open http://foo.se'),
-    (Command('open foo.io'), 'open http://foo.io')])
+    (Command('open foo.io'), 'open http://foo.io'),
+    (Command('xdg-open foo.io'), 'xdg-open http://foo.io'),
+    (Command('gnome-open foo.io'), 'gnome-open http://foo.io'),
+    (Command('kde-open foo.io'), 'kde-open http://foo.io')])
 def test_get_new_command(command, new_command):
     assert get_new_command(command, None) == new_command
