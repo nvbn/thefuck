42a8b4f639269886e468762e6d100b6f01aad8ab
Michael Lee <michael.lee@reporo.com>
2015-08-21 15:48:54 +0100

    Support for hdfs dfs -rm /directory

--- tests/rules/test_rm_dir.py
+++ tests/rules/test_rm_dir.py
@@ -5,17 +5,28 @@ from tests.utils import Command
 
 @pytest.mark.parametrize('command', [
     Command('rm foo', stderr='rm: foo: is a directory'),
-    Command('rm foo', stderr='rm: foo: Is a directory')])
+    Command('rm foo', stderr='rm: foo: Is a directory'),
+    Command('hdfs dfs -rm foo', stderr='rm: `foo`: Is a directory'),
+    Command('./bin/hdfs dfs -rm foo', stderr='rm: `foo`: Is a directory')
+    ])
 def test_match(command):
     assert match(command, None)
     assert match(command, None)
 
 
 @pytest.mark.parametrize('command', [
-    Command('rm foo'), Command('rm foo'), Command()])
+    Command('rm foo'), 
+    Command('hdfs dfs -rm foo'),
+    Command('./bin/hdfs dfs -rm foo'),  
+    Command()])
 def test_not_match(command):
     assert not match(command, None)
 
 
-def test_get_new_command():
-    assert get_new_command(Command('rm foo', '', ''), None) == 'rm -rf foo'
+@pytest.mark.parametrize('command, new_command', [
+    (Command('rm foo'), 'rm -rf foo'),
+    (Command('hdfs dfs -rm foo'), 'hdfs dfs -rm -r foo')])
+def test_get_new_command(command, new_command):
+    assert get_new_command(command, None) == new_command
+
+
