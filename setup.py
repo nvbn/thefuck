faa7ee603057fa98c25507d30180c055d10d13d4
nvbn <nvbn.rm@gmail.com>
2015-09-05 11:21:21 +0300

    Bump to 2.9

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ elif (3, 0) < version < (3, 3):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '2.8'
+VERSION = '2.9'
 
 install_requires = ['psutil', 'colorama', 'six', 'decorator']
 extras_require = {':python_version<"3.4"': ['pathlib']}
