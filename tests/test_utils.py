dd0667ea8f74a3e762dd981215667a73a18e6331
nvbn <nvbn.rm@gmail.com>
2015-09-06 13:37:48 +0300

    #356 Ignore `thefuck` entry points

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -53,7 +53,7 @@ def get_aliases(mocker):
 
 
 @pytest.mark.usefixtures('no_memoize', 'get_aliases')
-def test_get_all_callables():
+def test_get_all_executables():
     all_callables = get_all_executables()
     assert 'vim' in all_callables
     assert 'fsck' in all_callables
