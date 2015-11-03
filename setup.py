89f868c11599c6017ad5de51275a45f064649622
nvbn <nvbn.rm@gmail.com>
2015-11-03 17:38:07 +0800

    Bump to 3.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ elif (3, 0) < version < (3, 3):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '3.1'
+VERSION = '3.2'
 
 install_requires = ['psutil', 'colorama', 'six', 'decorator']
 extras_require = {':python_version<"3.4"': ['pathlib']}
