fa1edd4baef2e37bc8d99dccf1f23e70b6999f9c
nvbn <nvbn.rm@gmail.com>
2016-03-23 05:12:29 +0200

    Bump to 3.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ elif (3, 0) < version < (3, 3):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '3.6'
+VERSION = '3.7'
 
 install_requires = ['psutil', 'colorama', 'six', 'decorator']
 extras_require = {':python_version<"3.4"': ['pathlib'],
