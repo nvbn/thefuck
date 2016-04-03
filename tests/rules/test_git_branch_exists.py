0c98053f743f8fcf6c8e248827b08e52a1ee13a4
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-04-02 23:57:43 -0300

    #N/A Add a new rule `git_branch_exists`

---
+++ tests/rules/test_git_branch_exists.py
@@ -0,0 +1,33 @@
+import pytest
+from thefuck.rules.git_branch_exists import match, get_new_command
+from tests.utils import Command
+
+
+@pytest.fixture
+def stderr(branch_name):
+    return "fatal: A branch named '{}' already exists.".format(branch_name)
+
+
+@pytest.fixture
+def new_command(branch_name):
+    return [cmd.format(branch_name) for cmd in [
+        'git branch -d {0} && git branch {0}',
+        'git branch -D {0} && git branch {0}', 'git checkout {0}']]
+
+
+@pytest.mark.parametrize('script, branch_name', [
+    ('git branch foo', 'foo'),
+    ('git branch bar', 'bar')])
+def test_match(stderr, script, branch_name):
+    assert match(Command(script=script, stderr=stderr))
+
+
+@pytest.mark.parametrize('script', ['git branch foo', 'git branch bar'])
+def test_not_match(script):
+    assert not match(Command(script=script, stderr=''))
+
+
+@pytest.mark.parametrize('script, branch_name, ', [
+    ('git branch foo', 'foo'), ('git branch bar', 'bar')])
+def test_get_new_command(stderr, new_command, script, branch_name):
+    assert get_new_command(Command(script=script, stderr=stderr)) == new_command
