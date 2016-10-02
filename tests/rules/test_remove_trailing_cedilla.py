d05eb0a6dc0902793adff3bbf182729c95513e5a
Vladimir Iakovlev <nvbn.rm@gmail.com>
2016-10-02 17:19:33 +0200

    #552: Fix code style

--- tests/rules/test_remove_trailing_cedilla.py
+++ tests/rules/test_remove_trailing_cedilla.py
@@ -2,11 +2,13 @@ import pytest
 from thefuck.rules.remove_trailing_cedilla import match, get_new_command, CEDILLA
 from tests.utils import Command
 
+
 @pytest.mark.parametrize('command', [
     Command(script='wrong' + CEDILLA),
     Command(script='wrong with args' + CEDILLA)])
 def test_match(command):
     assert match(command)
+
 
 @pytest.mark.parametrize('command, new_command', [
     (Command('wrong' + CEDILLA), 'wrong'),
--- thefuck/rules/remove_trailing_cedilla.py
+++ thefuck/rules/remove_trailing_cedilla.py
@@ -1,9 +1,11 @@
-# encoding=utf8
+# -*- encoding: utf-8 -*-
 
 CEDILLA = u"ç"
 
+
 def match(command):
     return command.script.endswith(CEDILLA)
+
 
 def get_new_command(command):
     return command.script[:-1]
