ce6b82c92d78ae283cb3db001766b76f6647bc47
Vladimir Iakovlev <nvbn.rm@gmail.com>
2016-10-03 13:07:30 +0200

    #560: Fix code style

--- thefuck/rules/git_diff_no_index.py
+++ thefuck/rules/git_diff_no_index.py
@@ -1,19 +1,16 @@
-from six.moves import filterfalse
 from thefuck.utils import replace_argument
 from thefuck.specific.git import git_support
 
 
 @git_support
 def match(command):
-    args = command.script_parts[2:]
-    files = list(filterfalse(is_option, args))
-    return ('diff' in command.script and
-            '--no-index' not in command.script and
-            len(command.stdout) is 0 and
-            len(files) is 2)
+    files = [arg for arg in command.script_parts[2:]
+             if not arg.startswith('-')]
+    return ('diff' in command.script
+            and '--no-index' not in command.script
+            and not command.stdout
+            and len(files) == 2)
 
-def is_option(script_part):
-    return script_part.startswith('-')
 
 @git_support
 def get_new_command(command):
