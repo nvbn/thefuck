a964af7e9534edae3808acde97de054c3c6dde22
Joseph Frazier <1212jtraceur@gmail.com>
2016-10-05 10:55:49 -0400

    ag_literal.py: use `endswith()` rather than `in`
    https://github.com/nvbn/thefuck/pull/561#discussion_r81898499

--- thefuck/rules/ag_literal.py
+++ thefuck/rules/ag_literal.py
@@ -3,7 +3,7 @@ from thefuck.utils import for_app
 
 @for_app('ag')
 def match(command):
-    return 'run ag with -Q' in command.stderr
+    return command.stderr.endswith('run ag with -Q\n')
 
 
 def get_new_command(command):
