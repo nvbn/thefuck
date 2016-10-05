93302c74b5d73256e439c4556af0780f82f60cb6
Joseph Frazier <1212jtraceur@gmail.com>
2016-10-05 11:12:06 -0400

    Fix flake8 errors: E101/W191 indentation contains (mixed spaces and) tabs

--- tests/rules/test_brew_link.py
+++ tests/rules/test_brew_link.py
@@ -6,15 +6,15 @@ from thefuck.rules.brew_link import get_new_command, match
 @pytest.fixture
 def stderr():
     return ("Error: Could not symlink bin/gcp\n"
-			"Target /usr/local/bin/gcp\n"
-			"already exists. You may want to remove it:\n"
-			  "rm '/usr/local/bin/gcp'\n"
-			"\n"
-			"To force the link and overwrite all conflicting files:\n"
-			  "brew link --overwrite coreutils\n"
-			"\n"
-			"To list all files that would be deleted:\n"
-			  "brew link --overwrite --dry-run coreutils\n")
+            "Target /usr/local/bin/gcp\n"
+            "already exists. You may want to remove it:\n"
+              "rm '/usr/local/bin/gcp'\n"
+            "\n"
+            "To force the link and overwrite all conflicting files:\n"
+              "brew link --overwrite coreutils\n"
+            "\n"
+            "To list all files that would be deleted:\n"
+              "brew link --overwrite --dry-run coreutils\n")
 
 
 @pytest.fixture
