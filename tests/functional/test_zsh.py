16a440cb9d3476b71e9e7ebd975a16ee609159f5
Joseph Frazier <1212jtraceur@gmail.com>
2016-10-06 11:15:16 -0400

    test_zsh.py: use official python images, not ubuntu
    This should help reduce build times.

--- tests/functional/test_zsh.py
+++ tests/functional/test_zsh.py
@@ -3,19 +3,14 @@ from tests.functional.plots import with_confirmation, without_confirmation, \
     refuse_with_confirmation, history_changed, history_not_changed, \
     select_command_with_arrows, how_to_configure
 
-containers = (('thefuck/ubuntu-python3-zsh',
-               u'''FROM ubuntu:latest
+containers = (('thefuck/python3-zsh',
+               u'''FROM python:3
                    RUN apt-get update
-                   RUN apt-get install -yy python3 python3-pip python3-dev git
-                   RUN pip3 install -U setuptools
-                   RUN ln -s /usr/bin/pip3 /usr/bin/pip
                    RUN apt-get install -yy zsh''',
                u'zsh'),
-              ('thefuck/ubuntu-python2-zsh',
-               u'''FROM ubuntu:latest
+              ('thefuck/python2-zsh',
+               u'''FROM python:2
                    RUN apt-get update
-                   RUN apt-get install -yy python python-pip python-dev git
-                   RUN pip2 install -U pip setuptools
                    RUN apt-get install -yy zsh''',
                u'zsh'))
 
