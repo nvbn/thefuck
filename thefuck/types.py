1b30c00546479b56d242ab6e95417cb81203cca6
Joseph Frazier <1212jtraceur@gmail.com>
2016-10-06 14:03:15 -0400

    Fix flake8 errors: E122 continuation line missing indentation or outdented

--- thefuck/types.py
+++ thefuck/types.py
@@ -171,9 +171,9 @@ class Rule(object):
         return 'Rule(name={}, match={}, get_new_command={}, ' \
                'enabled_by_default={}, side_effect={}, ' \
                'priority={}, requires_output)'.format(
-            self.name, self.match, self.get_new_command,
-            self.enabled_by_default, self.side_effect,
-            self.priority, self.requires_output)
+                    self.name, self.match, self.get_new_command,
+                    self.enabled_by_default, self.side_effect,
+                    self.priority, self.requires_output)
 
     @classmethod
     def from_path(cls, path):
