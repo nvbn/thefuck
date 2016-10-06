10b20574d1f462ed9487f429fcea43136ac77803
Joseph Frazier <1212jtraceur@gmail.com>
2016-10-06 11:12:36 -0400

    test_tcsh.py: use official python images, not ubuntu
    This should help reduce build times.

--- tests/functional/test_tcsh.py
+++ tests/functional/test_tcsh.py
@@ -2,19 +2,14 @@ import pytest
 from tests.functional.plots import with_confirmation, without_confirmation, \
     refuse_with_confirmation, select_command_with_arrows
 
-containers = (('thefuck/ubuntu-python3-tcsh',
-               u'''FROM ubuntu:latest
+containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
                    RUN apt-get update
-                   RUN apt-get install -yy python3 python3-pip python3-dev git
-                   RUN pip3 install -U setuptools
-                   RUN ln -s /usr/bin/pip3 /usr/bin/pip
                    RUN apt-get install -yy tcsh''',
                u'tcsh'),
-              ('thefuck/ubuntu-python2-tcsh',
-               u'''FROM ubuntu:latest
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
                    RUN apt-get update
-                   RUN apt-get install -yy python python-pip python-dev git
-                   RUN pip2 install -U pip setuptools
                    RUN apt-get install -yy tcsh''',
                u'tcsh'))
 
