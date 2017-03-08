78515c7bbb81263fa339a67c2aabfa1a4f3c9af9
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-08 12:43:34 -0500

    Fix flake8 errors: W391 blank line at end of file

--- thefuck/rules/ifconfig_device_not_found.py
+++ thefuck/rules/ifconfig_device_not_found.py
@@ -21,5 +21,3 @@ def get_new_command(command):
     interface = command.stderr.split(' ')[0][:-1]
     possible_interfaces = _get_possible_interfaces()
     return replace_command(command, interface, possible_interfaces)
-
-
