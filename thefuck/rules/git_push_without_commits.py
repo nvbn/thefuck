b22a3ac891b2610d596225ba05dbd0729b707fb0
Adam Barnes <adam@jp74.com>
2017-03-22 10:23:35 +0000

    Created a rule for trying to push a new repository with no commits.

---
+++ thefuck/rules/git_push_without_commits.py
@@ -0,0 +1,17 @@
+import re
+
+from thefuck.utils import replace_command
+
+fix = 'git commit -m "Initial commit." && {command}'
+refspec_does_not_match = re.compile(r'src refspec \w+ does not match any\.')
+
+
+def match(command):
+    if refspec_does_not_match.search(command.stderr):
+        return True
+
+    return False
+
+
+def get_new_command(command):
+    return fix.format(command=command.script)
