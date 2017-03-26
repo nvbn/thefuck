2f163936584e7dfb6a59dbf3bbef9bd21b44e15f
Russ Panula <dfadev@dfa.net>
2017-03-26 10:12:45 -0700

    style

--- thefuck/rules/yarn_command_replaced.py
+++ thefuck/rules/yarn_command_replaced.py
@@ -3,9 +3,11 @@ from thefuck.utils import for_app
 
 regex = re.compile(r'Run "(.*)" instead')
 
+
 @for_app('yarn', at_least=1)
 def match(command):
     return regex.findall(command.stderr)
+
 
 def get_new_command(command):
     return regex.findall(command.stderr)[0]
