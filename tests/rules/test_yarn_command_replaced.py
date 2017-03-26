900e83e0280fd7d90f989608713a1a21a12853b9
Russ Panula <dfadev@dfa.net>
2017-03-25 23:12:01 -0700

    add rule for: yarn install [pkg]
    --- `install` has been replaced with `add` to add new dependencies. Run $0 instead.
    
    https://github.com/yarnpkg/yarn/blob/6e9a9a6596ca8f177f68f6672a1ef4ff16705336/src/reporters/lang/en.js#L18

---
+++ tests/rules/test_yarn_command_replaced.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+
+import pytest
+from tests.utils import Command
+from thefuck.rules.yarn_command_replaced import match, get_new_command
+
+
+stderr = '''
+error `install` has been replaced with `add` to add new dependencies. Run "yarn add {}" instead.
+'''.format
+
+
+@pytest.mark.parametrize('command', [
+    Command(script='yarn install asdklj', stderr=stderr('asdklj')),
+    Command(script='yarn install liuqowe', stderr=stderr('liuqowe')),
+    Command(script='yarn install zxmnc', stderr=stderr('zxmnc'))])
+def test_match(command):
+    assert match(command)
+
+
+@pytest.mark.parametrize('command, new_command', [
+    (Command('yarn install asdklj', stderr=stderr('asdklj')), 'yarn add asdklj'),
+    (Command('yarn install iiuqowe', stderr=stderr('iiuqowe')), 'yarn add iiuqowe'),
+    (Command('yarn install zxmnc', stderr=stderr('zxmnc')), 'yarn add zxmnc')])
+def test_get_new_command(command, new_command):
+    assert get_new_command(command) == new_command
+
+
+@pytest.mark.parametrize('command', [
+    Command('yarn install')])
+def test_not_match(command):
+    assert not match(command)
