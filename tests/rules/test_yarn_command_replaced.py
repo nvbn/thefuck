802fcd96fdb328205fb28658afca6b814c24355f
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-03-28 18:35:40 +0200

    #621: Refine `yarn_command_replaced` rule tests

--- tests/rules/test_yarn_command_replaced.py
+++ tests/rules/test_yarn_command_replaced.py
@@ -1,32 +1,32 @@
-# -*- encoding: utf-8 -*-
-
 import pytest
 from tests.utils import Command
 from thefuck.rules.yarn_command_replaced import match, get_new_command
 
 
-stderr = '''
-error `install` has been replaced with `add` to add new dependencies. Run "yarn add {}" instead.
-'''.format
+stderr = ('error `install` has been replaced with `add` to add new '
+          'dependencies. Run "yarn add {}" instead.').format
 
 
 @pytest.mark.parametrize('command', [
-    Command(script='yarn install asdklj', stderr=stderr('asdklj')),
-    Command(script='yarn install liuqowe', stderr=stderr('liuqowe')),
-    Command(script='yarn install zxmnc', stderr=stderr('zxmnc'))])
+    Command(script='yarn install redux', stderr=stderr('redux')),
+    Command(script='yarn install moment', stderr=stderr('moment')),
+    Command(script='yarn install lodash', stderr=stderr('lodash'))])
 def test_match(command):
     assert match(command)
-
-
-@pytest.mark.parametrize('command, new_command', [
-    (Command('yarn install asdklj', stderr=stderr('asdklj')), 'yarn add asdklj'),
-    (Command('yarn install iiuqowe', stderr=stderr('iiuqowe')), 'yarn add iiuqowe'),
-    (Command('yarn install zxmnc', stderr=stderr('zxmnc')), 'yarn add zxmnc')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
 
 
 @pytest.mark.parametrize('command', [
     Command('yarn install')])
 def test_not_match(command):
     assert not match(command)
+
+
+@pytest.mark.parametrize('command, new_command', [
+    (Command('yarn install redux', stderr=stderr('redux')),
+     'yarn add redux'),
+    (Command('yarn install moment', stderr=stderr('moment')),
+     'yarn add moment'),
+    (Command('yarn install lodash', stderr=stderr('lodash')),
+     'yarn add lodash')])
+def test_get_new_command(command, new_command):
+    assert get_new_command(command) == new_command
