d71dbc5de4d34b48cb68c8b1671b6e39d83e4e67
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-08-21 11:55:34 +0200

    #678: Speedup `fuck` by hardcoding entry points

--- thefuck/utils.py
+++ thefuck/utils.py
@@ -107,9 +107,7 @@ def get_all_executables():
             return fallback
 
     tf_alias = get_alias()
-    tf_entry_points = get_installation_info().get_entry_map()\
-                                             .get('console_scripts', {})\
-                                             .keys()
+    tf_entry_points = ['thefuck', 'fuck']
 
     bins = [exe.name.decode('utf8') if six.PY2 else exe.name
             for path in os.environ.get('PATH', '').split(':')
