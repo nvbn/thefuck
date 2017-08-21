6c4333944f701dc6f792594e0046e5a07ea645de
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-08-21 12:26:19 +0200

    Bump to 3.21

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ elif (3, 0) < version < (3, 3):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '3.20'
+VERSION = '3.21'
 
 install_requires = ['psutil', 'colorama', 'six', 'decorator']
 extras_require = {':python_version<"3.4"': ['pathlib2'],
