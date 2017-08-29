db12211e05abe542de9bf14029d8c13985cf50e2
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-08-29 09:39:32 +0200

    Bump to 3.23

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ elif (3, 0) < version < (3, 3):
           ' ({}.{} detected).'.format(*version))
     sys.exit(-1)
 
-VERSION = '3.22'
+VERSION = '3.23'
 
 install_requires = ['psutil', 'colorama', 'six', 'decorator', 'pyte']
 extras_require = {':python_version<"3.4"': ['pathlib2'],
