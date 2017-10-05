89bc2e9759075ea851eef2ef118ab1cbc4db38d2
Stef Pletinck <s@stefpletinck.be>
2017-10-05 18:44:04 +0200

    fixed #670

---
+++ thefuck/rules/git_remote_delete.py
@@ -0,0 +1,12 @@
+from thefuck.utils import replace_argument
+from thefuck.specific.git import git_support
+
+
+@git_support
+def match(command):
+    return "git remote delete" in command.script
+
+
+@git_support
+def get_new_command(command):
+    return replace_argument(command.script, "delete", "remove")
