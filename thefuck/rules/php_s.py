2ffb7b9afb5d5ca4d51ba42f545f56ccb4727ed7
Stef Pletinck <s@stefpletinck.be>
2017-10-07 14:30:08 +0200

    Enabled by default and fix

--- thefuck/rules/php_s.py
+++ thefuck/rules/php_s.py
@@ -1,9 +1,14 @@
-from thefuck.utils import replace_argument
+from thefuck.utils import replace_argument, for_app
 
 
+@for_app('php')
 def match(command):
     return "php -s" in command.script
 
 
 def get_new_command(command):
     return replace_argument(command.script, "-s", "-S")
+
+
+enabled_by_default = True
+requires_output = False
