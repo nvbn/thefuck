75d2c43997ca703150cbdb4c46ed7b2e2e71fd11
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2017-10-10 14:48:47 -0300

    #702: fix minor issues with `php_s` rule
    Unfortunately, I didn't catch these issues while reviewing #702.
    
    After looking more closely at `php` options, `-S` requires additional
    arguments (<address>:<port>) and `-s` may produce output if used that
    way. So, matching ` -s ` seems to be better.
    
    Also, `@for_app('php')` already asserts the presence of `php ` in the
    command script. Matching `php -s` prevents the rule from fixing commands
    like `php -t public -s 0.0.0.0:8080`.

--- thefuck/rules/php_s.py
+++ thefuck/rules/php_s.py
@@ -3,11 +3,8 @@ from thefuck.utils import replace_argument, for_app
 
 @for_app('php')
 def match(command):
-    return "php -s" in command.script
+    return " -s " in command.script
 
 
 def get_new_command(command):
     return replace_argument(command.script, "-s", "-S")
-
-
-requires_output = False
