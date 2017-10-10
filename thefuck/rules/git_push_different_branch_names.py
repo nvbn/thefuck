e406ef31a0707a0e9a35e3ee48bd5b1676ddabc3
Stef Pletinck <s@stefpletinck.be>
2017-10-10 13:35:57 +0200

    Removed redundant lines

--- thefuck/rules/git_push_different_branch_names.py
+++ thefuck/rules/git_push_different_branch_names.py
@@ -4,12 +4,9 @@ from thefuck.specific.git import git_support
 
 @git_support
 def match(command):
-    return "git push" in command.script and "The upstream branch of your current branch does not match" in command.output
+    return "push" in command.script and "The upstream branch of your current branch does not match" in command.output
 
 
 @git_support
 def get_new_command(command):
     return re.findall(r'^ +(git push [^\s]+ [^\s]+)', command.output, re.MULTILINE)[0]
-
-
-enabled_by_default = True
