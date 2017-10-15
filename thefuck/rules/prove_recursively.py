a566e040f76f39296161b15d471a858cdb60ee47
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-10-15 16:16:43 +0200

    #N/A: Fix deprecation warnings

--- thefuck/rules/prove_recursively.py
+++ thefuck/rules/prove_recursively.py
@@ -16,7 +16,7 @@ def _isdir(part):
 @for_app('prove')
 def match(command):
     return (
-        'NOTESTS' in command.stdout
+        'NOTESTS' in command.output
         and not any(_is_recursive(part) for part in command.script_parts[1:])
         and any(_isdir(part) for part in command.script_parts[1:]))
 
