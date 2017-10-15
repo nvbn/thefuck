bab5de236f4a9a44b033bc563198e8d188733c9f
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-10-15 16:39:40 +0200

    #710: Fix tests

--- tests/rules/test_brew_update_formula.py
+++ tests/rules/test_brew_update_formula.py
@@ -3,28 +3,26 @@ from thefuck.types import Command
 from thefuck.rules.brew_update_formula import get_new_command, match
 
 
-@pytest.fixture
-def output():
-    return ("Error: This command updates brew itself, and does not take formula"
-            " names.\nUse 'brew upgrade <formula>'.")
+output = ("Error: This command updates brew itself, and does not take formula"
+          " names.\nUse 'brew upgrade thefuck'.")
 
 
-@pytest.fixture
-def new_command(formula):
-    return 'brew upgrade {}'.format(formula)
+def test_match():
+    command = Command('brew update thefuck', output)
+    assert match(command)
 
 
-@pytest.mark.parametrize('script', ['brew update foo', 'brew update bar zap'])
-def test_match(output, script):
-    assert match(Command(script, output))
-
-
-@pytest.mark.parametrize('script', ['brew upgrade foo', 'brew update'])
+@pytest.mark.parametrize('script', [
+    'brew upgrade foo',
+    'brew update'])
 def test_not_match(script):
     assert not match(Command(script, ''))
 
 
 @pytest.mark.parametrize('script, formula, ', [
-    ('brew update foo', 'foo'), ('brew update bar zap', 'bar zap')])
-def test_get_new_command(output, new_command, script, formula):
-    assert get_new_command(Command(script, output)) == new_command
+    ('brew update foo', 'foo'),
+    ('brew update bar zap', 'bar zap')])
+def test_get_new_command(script, formula):
+    command = Command(script, output)
+    new_command = 'brew upgrade {}'.format(formula)
+    assert get_new_command(command) == new_command
