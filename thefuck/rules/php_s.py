054fd0b166782d30af5723002600a30c59dd87dd
Vladimir Iakovlev <nvbn.rm@gmail.com>
2017-10-16 19:09:44 +0200

    #702: Make `match` in `php_s` rule more strict

--- thefuck/rules/php_s.py
+++ thefuck/rules/php_s.py
@@ -1,9 +1,10 @@
 from thefuck.utils import replace_argument, for_app
 
 
-@for_app('php')
+@for_app('php', at_least=2)
 def match(command):
-    return " -s " in command.script
+    return ('-s' in command.script_parts
+            and command.script_parts[-1] != '-s')
 
 
 def get_new_command(command):
