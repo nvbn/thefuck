b4aa9557459a1f547379c78bc83d8447cc5b7787
David <davidhart82@gmail.com>
2018-01-02 22:18:46 +0000

    Fix IndexError when using bitbucket

--- tests/rules/test_git_push.py
+++ tests/rules/test_git_push.py
@@ -13,11 +13,28 @@ To push the current branch and set the remote as upstream, use
 '''
 
 
+@pytest.fixture
+def output_bitbucket():
+    return '''Total 0 (delta 0), reused 0 (delta 0)
+remote:
+remote: Create pull request for feature/test:
+remote:   https://bitbucket.org/test.git
+remote:
+To git@bitbucket.org:test.git
+   e5e7fbb..700d998  feature/test -> feature/test
+Branch feature/test set up to track remote branch feature/test from origin.
+'''
+
+
 def test_match(output):
     assert match(Command('git push', output))
     assert match(Command('git push master', output))
     assert not match(Command('git push master', ''))
     assert not match(Command('ls', output))
+
+
+def test_match_bitbucket(output_bitbucket):
+    assert not match(Command('git push origin', output_bitbucket))
 
 
 def test_get_new_command(output):
