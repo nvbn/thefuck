7e81036d940006231e90cd0eaf94cb48325d134f
David <davidhart82@gmail.com>
2018-01-03 20:27:45 +0000

    Set branch name to something much more dangerous for old match

--- tests/rules/test_git_push.py
+++ tests/rules/test_git_push.py
@@ -17,12 +17,12 @@ To push the current branch and set the remote as upstream, use
 def output_bitbucket():
     return '''Total 0 (delta 0), reused 0 (delta 0)
 remote:
-remote: Create pull request for feature/test:
-remote:   https://bitbucket.org/test
+remote: Create pull request for feature/set-upstream:
+remote:   https://bitbucket.org/set-upstream
 remote:
 To git@bitbucket.org:test.git
-   e5e7fbb..700d998  feature/test -> feature/test
-Branch feature/test set up to track remote branch feature/test from origin.
+   e5e7fbb..700d998  feature/set-upstream -> feature/set-upstream
+Branch feature/set-upstream set up to track remote branch feature/set-upstream from origin.
 '''
 
 
