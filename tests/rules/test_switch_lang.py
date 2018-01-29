b65e3a9aaded6c7f223597794f5b3711483aeffc
Omer Katz <omer.drow@gmail.com>
2018-01-29 09:46:18 +0200

    Added hebrew the list of keyboard layouts (#778)
    * Added hebrew the list of keyboard layouts.
    
    Fixes #776.
    
    * Added tests for hebrew layout.
    
    * Fix test.
    
    * Make lint happy.

--- tests/rules/test_switch_lang.py
+++ tests/rules/test_switch_lang.py
@@ -7,7 +7,9 @@ from thefuck.types import Command
 
 @pytest.mark.parametrize('command', [
     Command(u'фзе-пуе', 'command not found: фзе-пуе'),
-    Command(u'λσ', 'command not found: λσ')])
+    Command(u'λσ', 'command not found: λσ'),
+    Command(u'שפא-עקא', 'command not found: שפא-עקא'),
+    Command(u'ךד', 'command not found: ךד')])
 def test_match(command):
     assert switch_lang.match(command)
 
@@ -16,13 +18,16 @@ def test_match(command):
     Command(u'pat-get', 'command not found: pat-get'),
     Command(u'ls', 'command not found: ls'),
     Command(u'агсл', 'command not found: агсл'),
-    Command(u'фзе-пуе', 'some info')])
+    Command(u'фзе-пуе', 'some info'),
+    Command(u'שפא-עקא', 'some info')])
 def test_not_match(command):
     assert not switch_lang.match(command)
 
 
 @pytest.mark.parametrize('command, new_command', [
     (Command(u'фзе-пуе штыефдд мшь', ''), 'apt-get install vim'),
-    (Command(u'λσ -λα', ''), 'ls -la')])
+    (Command(u'λσ -λα', ''), 'ls -la'),
+    (Command(u'שפא-עקא ןמדאשךך הןצ', ''), 'apt-get install vim'),
+    (Command(u'ךד -ךש', ''), 'ls -la')])
 def test_get_new_command(command, new_command):
     assert switch_lang.get_new_command(command) == new_command
