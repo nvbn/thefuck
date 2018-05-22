81042514c8204d159283e85651d35c20f20c7863
Vladimir Iakovlev <nvbn.rm@gmail.com>
2018-05-22 19:01:51 +0200

    Squashed commit of the following:
    commit 6919161e77a39b9bd59ca54eac44b956cd3ae1dc
    Author: Vladimir Iakovlev <nvbn.rm@gmail.com>
    Date:   Tue May 22 19:01:33 2018 +0200
    
        #810: Fix code style
    
    commit ebbb31a3227ce32ba5288e96c0c16a3d334c45d6
    Merge: 2df1a5a a2799ad
    Author: Vladimir Iakovlev <nvbn.rm@gmail.com>
    Date:   Tue May 22 18:59:56 2018 +0200
    
        Merge branch 'feature/long-form-help' of https://github.com/jakewarren/thefuck into jakewarren-feature/long-form-help
    
    commit a2799ad09894808fc23ef1a99475ca30c3d4d67c
    Author: Jake Warren <jakewarren@users.noreply.github.com>
    Date:   Mon May 7 14:12:57 2018 -0500
    
        Add new `long_form_help` rule

---
+++ thefuck/rules/long_form_help.py
@@ -0,0 +1,27 @@
+from thefuck.utils import replace_argument
+import re
+
+# regex to match a suggested help command from the tool output
+help_regex = r"(?:Run|Try) '([^']+)'(?: or '[^']+')? for (?:details|more information)."
+
+
+def match(command):
+    if re.search(help_regex, command.output, re.I) is not None:
+        return True
+
+    if '--help' in command.output:
+        return True
+
+    return False
+
+
+def get_new_command(command):
+    if re.search(help_regex, command.output) is not None:
+        match_obj = re.search(help_regex, command.output, re.I)
+        return match_obj.group(1)
+
+    return replace_argument(command.script, '-h', '--help')
+
+
+enabled_by_default = True
+priority = 5000
