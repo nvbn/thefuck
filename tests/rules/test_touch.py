cc6d90963e38ec1882bd1fd8529a2396d0b11387
Pablo Aguiar <scorphus@gmail.com>
2018-07-29 17:18:42 +0100

    #367: Support BSD style output in touch rule (#830)
    On a Mac, also on NetBSD or OpenBSD, `touch` errs differently:
    
    ```
    $ uname; touch a/b/c
    Darwin
    touch: a/b/c: No such file or directory
    ```
    
    That gets matched by the rule but not fixed by it. Thus the regex
    pattern is now a bit more tolerant.

--- tests/rules/test_touch.py
+++ tests/rules/test_touch.py
@@ -4,24 +4,32 @@ from thefuck.types import Command
 
 
 @pytest.fixture
-def output():
-    return "touch: cannot touch '/a/b/c':" \
-           " No such file or directory"
+def output(is_bsd):
+    print(is_bsd)
+    if is_bsd:
+        return "touch: /a/b/c: No such file or directory"
+    return "touch: cannot touch '/a/b/c': No such file or directory"
 
 
-def test_match(output):
-    command = Command('touch /a/b/c', output)
+@pytest.mark.parametrize('script, is_bsd', [
+    ('touch /a/b/c', False),
+    ('touch /a/b/c', True)])
+def test_match(script, is_bsd, output):
+    command = Command(script, output)
     assert match(command)
 
 
 @pytest.mark.parametrize('command', [
     Command('touch /a/b/c', ''),
-    Command('ls /a/b/c', output())])
+    Command('ls /a/b/c', output(False))])
 def test_not_match(command):
     assert not match(command)
 
 
-def test_get_new_command(output):
-    command = Command('touch /a/b/c', output)
+@pytest.mark.parametrize('script, is_bsd', [
+    ('touch /a/b/c', False),
+    ('touch /a/b/c', True)])
+def test_get_new_command(script, is_bsd, output):
+    command = Command(script, output)
     fixed_command = get_new_command(command)
     assert fixed_command == 'mkdir -p /a/b && touch /a/b/c'
