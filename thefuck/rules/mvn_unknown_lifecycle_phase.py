087c58f21b35869a267a1014a2024859c63777ba
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-09-29 22:59:01 +0200

    #N/A: omit default arguments to get_close_matches

--- thefuck/rules/mvn_unknown_lifecycle_phase.py
+++ thefuck/rules/mvn_unknown_lifecycle_phase.py
@@ -25,8 +25,7 @@ def get_new_command(command):
     available_lifecycles = _getavailable_lifecycles(command)
     if available_lifecycles and failed_lifecycle:
         selected_lifecycle = get_close_matches(
-            failed_lifecycle.group(1), available_lifecycles.group(1).split(", "),
-            3, 0.6)
+            failed_lifecycle.group(1), available_lifecycles.group(1).split(", "))
         return replace_command(command, failed_lifecycle.group(1), selected_lifecycle)
     else:
         return []
