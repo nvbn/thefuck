d86dd5f179376f273c6005b1091676c198b44afe
Vladimir Iakovlev <nvbn.rm@gmail.com>
2018-11-29 23:57:07 +0100

    #N/A: Clear `dist/` before uploading releases

--- release.py
+++ release.py
@@ -32,5 +32,6 @@ call('git push --tags', shell=True)
 
 env = os.environ
 env['CONVERT_README'] = 'true'
+call('rm -rf dist/*')
 call('python setup.py sdist bdist_wheel', shell=True, env=env)
 call('twine upload dist/*', shell=True, env=env)
