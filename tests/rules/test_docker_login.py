c41dfe842d180bd4e4293477b754f7d06b8fa33f
Inga Feick <inga.feick@trivago.com>
2019-03-21 21:56:00 +0100

    Add docker_login rule

---
+++ tests/rules/test_docker_login.py
@@ -0,0 +1,16 @@
+from thefuck.rules.docker_login import match, get_new_command
+from thefuck.types import Command
+
+
+def test_match():
+	err_response = """
+	Sending build context to Docker daemon  118.8kB
+Step 1/6 : FROM baz/baz:nightly
+pull access denied for baz/baz, repository does not exist or may require 'docker login'
+"""
+    assert match(Command('docker build -t artifactory:9090/foo/bar:fdb7c6d .', err_response))
+    assert not match(Command('', ''))
+
+
+def test_get_new_command():
+    assert get_new_command(Command('docker build -t artifactory:9090/foo/bar:fdb7c6d .', '')) == 'docker login && docker build -t artifactory:9090/foo/bar:fdb7c6d .'
