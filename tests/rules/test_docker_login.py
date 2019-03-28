e6a2220cec249992280b7c4b7156b4707f3f4ea3
Inga Feick <inga.feick@trivago.com>
2019-03-28 15:51:39 +0100

    Fix typo in test case

--- tests/rules/test_docker_login.py
+++ tests/rules/test_docker_login.py
@@ -5,7 +5,7 @@ from thefuck.types import Command
 def test_match():
     err_response = """
     Sending build context to Docker daemon  118.8kB
-Step 1/6 : FROM foo/bar:nightly
+Step 1/6 : FROM foo/bar:fdb7c6d
 pull access denied for foo/bar, repository does not exist or may require 'docker login'
 """
     assert match(Command('docker build -t artifactory:9090/foo/bar:fdb7c6d .', err_response))
