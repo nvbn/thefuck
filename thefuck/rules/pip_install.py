c34b65d3fb15cf37dbdcd82d14d7fec1e1348145
Inga Feick <inga.feick@trivago.com>
2019-04-09 18:07:54 +0200

    Switch quotation to single

--- thefuck/rules/pip_install.py
+++ thefuck/rules/pip_install.py
@@ -5,11 +5,11 @@ from thefuck.specific.sudo import sudo_support
 @sudo_support
 @for_app('pip')
 def match(command):
-    return ('pip install' in command.script and "Permission denied" in command.output)
+    return ('pip install' in command.script and 'Permission denied' in command.output)
 
 
 def get_new_command(command):
-    if "--user" not in command.script:  # add --user (attempt 1)
-        return command.script.replace(" install ", " install --user ")
+    if '--user' not in command.script:  # add --user (attempt 1)
+        return command.script.replace(' install ', ' install --user ')
     else:  # since --user didn't fix things, let's try sudo (attempt 2)
-        return 'sudo {}'.format(command.script.replace(" --user", ""))
+        return 'sudo {}'.format(command.script.replace(' --user', ''))
