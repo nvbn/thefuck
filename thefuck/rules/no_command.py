5efcf1019f90b75561bbe9f1109b83778d0dddeb
Pablo Aguiar <scorphus@gmail.com>
2019-05-27 18:23:06 +0200

    #N/A: Improve support to Windows in `no_command` rule (#918)
    Windows “not found” message is quite different from POSIX systems.

--- thefuck/rules/no_command.py
+++ thefuck/rules/no_command.py
@@ -6,7 +6,8 @@ from thefuck.specific.sudo import sudo_support
 @sudo_support
 def match(command):
     return (not which(command.script_parts[0])
-            and 'not found' in command.output
+            and ('not found' in command.output
+                 or 'is not recognized as' in command.output)
             and bool(get_close_matches(command.script_parts[0],
                                        get_all_executables())))
 
