0b92abe9d3f456bc77328eb3e0dc04869a53b9fb
Connor Martin <connormartin7@gmail.com>
2019-07-10 09:06:20 -0500

    test not docker command

--- tests/rules/test_docker_image_being_used_by_container.py
+++ tests/rules/test_docker_image_being_used_by_container.py
@@ -12,6 +12,11 @@ def test_not_match():
     assert not match(Command('docker image rm -f cd809b04b6ff', err_response))
 
 
+def test_not_docker_command():
+    err_response = """Error response from daemon: conflict: unable to delete cd809b04b6ff (cannot be forced) - image is being used by running container e5e2591040d1"""
+    assert not match(Command('git image rm -f cd809b04b6ff', err_response))
+
+
 def test_get_new_command():
     err_response = """
         Error response from daemon: conflict: unable to delete cd809b04b6ff (cannot be forced) - image
