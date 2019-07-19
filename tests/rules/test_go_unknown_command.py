17a7ea8fc44d5288933bc8411080eca2d04e0e20
ik1ne <ik1ne@naver.com>
2019-07-19 11:04:38 +0900

    Add: test_get_new_command for testing rules.go_unknown_command.test_get_new_command method.

--- tests/rules/test_go_unknown_command.py
+++ tests/rules/test_go_unknown_command.py
@@ -1,13 +1,21 @@
-from thefuck.rules.go_unknown_command import match
+import pytest
+from thefuck.rules.go_unknown_command import match, get_new_command
 from thefuck.types import Command
 
-_GO_BUILD_MISSPELLED_OUTPUT = """go bulid: unknown command
-Run 'go help' for usage."""
+
+@pytest.fixture
+def build_misspelled_output():
+    return '''go bulid: unknown command
+Run 'go help' for usage.'''
 
 
-def test_match():
-    assert match(Command('go bulid', _GO_BUILD_MISSPELLED_OUTPUT))
+def test_match(build_misspelled_output):
+    assert match(Command('go bulid', build_misspelled_output))
 
 
 def test_not_match():
     assert not match(Command('go run', 'go run: no go files listed'))
+
+
+def test_get_new_command(build_misspelled_output):
+    assert get_new_command(Command('go bulid', build_misspelled_output)) == 'go build'
