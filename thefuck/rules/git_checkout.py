84c16fb69a4c0abcb9fe1c90787076800320a950
ik1ne <3996272+ik1ne@users.noreply.github.com>
2019-09-03 02:16:40 +0900

    Change: rules_git_checkout handling branch names with slashes & Remote HEAD. (#944)
    * Add: Test for branch names with slashes & Remote HEAD.
    
    * - Add: Handling for removing remote HEAD.
    - Change: Improved handling for branches with slash in their names.

--- thefuck/rules/git_checkout.py
+++ thefuck/rules/git_checkout.py
@@ -18,10 +18,12 @@ def get_branches():
         stdout=subprocess.PIPE)
     for line in proc.stdout.readlines():
         line = line.decode('utf-8')
+        if '->' in line:    # Remote HEAD like b'  remotes/origin/HEAD -> origin/master'
+            continue
         if line.startswith('*'):
             line = line.split(' ')[1]
-        if '/' in line:
-            line = line.split('/')[-1]
+        if line.strip().startswith('remotes/'):
+            line = '/'.join(line.split('/')[2:])
         yield line.strip()
 
 
