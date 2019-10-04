7cc6a2e0d4576a2c5e96cb95173b64b7bb648cad
DragonGhost7 <darkiridiumghost@gmail.com>
2019-10-03 22:25:50 -0400

    Test fix

--- tests/rules/test_pacman_invalid_option.py
+++ tests/rules/test_pacman_invalid_option.py
@@ -1,17 +1,19 @@
-from thefuck.rules.pip_install import match, get_new_command
+import pytest
 from thefuck.types import Command
 
-
-def test_match():
-    right_response = """
-community/shared_meataxe 1.0-3
+good_output = """community/shared_meataxe 1.0-3
     A set of programs for working with matrix representations over finite fields
     """
-    assert not match(Command('pacman -Ss meat', right_response))
 
-    wrong_response = "error: invalid option '-s'"
-    assert match(Command('pacman -ss meat', wrong_response))
+bad_output = "error: invalid option '-s'"
 
+def test_match():
+    assert match(Command('pacman -Ss meat', good_output))
+    assert match(Command('sudo pacman -Ss meat', good_output))
 
 def test_get_new_command():
-    assert get_new_command(Command('pacman -s meat', '')) == 'pacman -S meat'
+    new_command = get_new_command(Command('pacman -ss meat', bad_output))
+    assert new_command == 'pacman -Ss meat'
+
+    new_command = get_new_command(Command('sudo pacman -s meat', bad_output))
+    assert new_command == 'sudo pacman -S meat'
