6fa078d55e13f0ea9945833c842c537c002b7c76
ik1ne <ik1ne@naver.com>
2019-10-08 20:03:01 +0900

    Add: yum_invalid_operation implementation.

--- thefuck/rules/yum_invalid_operation.py
+++ thefuck/rules/yum_invalid_operation.py
@@ -1,17 +1,36 @@
+import subprocess
+from itertools import dropwhile, islice, takewhile
+
 from thefuck.specific.sudo import sudo_support
-from thefuck.utils import for_app
+from thefuck.utils import for_app, replace_command, which, cache
 
 
 @sudo_support
 @for_app('yum')
 def match(command):
-    pass
+    return 'No such command: ' in command.output
 
 
 def _get_operations():
-    pass
+    proc = subprocess.Popen('yum', stdout=subprocess.PIPE)
+
+    lines = proc.stdout.readlines()
+    lines = [line.decode('utf-8') for line in lines]
+    lines = dropwhile(lambda line: not line.startswith("List of Commands:"), lines)
+    lines = islice(lines, 2, None)
+    lines = list(takewhile(lambda line: line.strip(), lines))
+    return [line.strip().split(' ')[0] for line in lines]
+
+
+if which('yum'):
+    _get_operations = cache(which('yum'))(_get_operations)
 
 
 @sudo_support
 def get_new_command(command):
-    pass
+    invalid_operation = command.script_parts[1]
+
+    if invalid_operation == 'uninstall':
+        return [command.script.replace('uninstall', 'remove')]
+
+    return replace_command(command, invalid_operation, _get_operations())
