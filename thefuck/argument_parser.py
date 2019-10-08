6789701e238499c3ef867dacd6aaffae9a6141e2
Fabian van Dijk <35370400+Faaab@users.noreply.github.com>
2019-10-08 23:44:22 +0200

    Add fuck --hard as alternative to fuck --yeah (#963)
    * Add fuck --hard as alternative to fuck --yeah
    
    * Fix missing comma in thefuck/argument_parser.py
    
    Co-Authored-By: lomckee <cstutoringluke@gmail.com>
    
    * Update README on fuck --hard

--- thefuck/argument_parser.py
+++ thefuck/argument_parser.py
@@ -55,7 +55,7 @@ class Parser(object):
         """It's too dangerous to use `-y` and `-r` together."""
         group = self._parser.add_mutually_exclusive_group()
         group.add_argument(
-            '-y', '--yes', '--yeah',
+            '-y', '--yes', '--yeah', '--hard',
             action='store_true',
             help='execute fixed command without confirmation')
         group.add_argument(
