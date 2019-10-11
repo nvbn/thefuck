cdaeb999c96d936d2b79e7b5eddb4dd6c663cfee
@amrit1207! <jainamrit233@gmail.com>
2019-10-11 09:19:24 +0530

    change in the code

--- thefuck/rules/aws_cli.py
+++ thefuck/rules/aws_cli.py
@@ -12,6 +12,6 @@ def match(command):
 
 
 def get_new_command(command):
-    mistake = re.search(INVALID_CHOICE, command.output).group(0)
-    options = re.findall(OPTIONS, command.output, flags=re.MULTILINE)
-    return [replace_argument(command.script, mistake, o) for o in options]
+    mi = re.search(INVALID_CHOICE, command.output).group(0)
+    op = re.findall(OPTIONS, command.output, flags=re.MULTILINE)
+    return [replace_argument(command.script, mi, k) for k in op]
