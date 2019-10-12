6eefea3ea5860bad442a637cc60f28012ee8817d
user <avi.salmon@intel.com>
2019-10-12 15:51:30 +0300

    Parsing manpages (still unoptimized)

--- thefuck/rules/wrong_hyphen_before_subcommand.py
+++ thefuck/rules/wrong_hyphen_before_subcommand.py
@@ -3,9 +3,12 @@ from thefuck.utils import get_all_executables, get_close_matches, \
 from thefuck.specific.sudo import sudo_support
 from thefuck import logs
 
+
 import os.path
 import subprocess
 import gzip
+import re
+
 
 def get_manpage_dir():
     return "/usr/share/man"
@@ -13,8 +16,8 @@ def get_manpage_dir():
 
 def get_manpage_path(command, number = None):
     if number == None:
-        return get_manpage_path(command, 1) or
-            get_manpage_path(command, 8) or
+        return get_manpage_path(command, 1) or\
+            get_manpage_path(command, 8) or\
             get_manpage_path(command, 7)
     
     path = os.path.join(get_manpage_dir(), "man%d" % (number,), "%s.%d.gz" % (command, number))
@@ -25,9 +28,27 @@ def get_manpage(command, number = None):
     path = get_manpage_path(command, number)
     if path:
         with gzip.open(path, "r") as f:
-            return [line.strip() for line in f.readlines()]
+            return list(filter(None, (textify(line) for line in f.readlines())))
     else:
         return []
+
+
+def textify(manline):
+    manline = re.sub(r"^\.[A-Za-z]+( |$)", "", str(manline, "utf-8").strip()) # Stripping line headers .SH, .B
+    manline = re.sub(r"\\f.", "", manline) # Stripping format
+    manline = re.sub(r"\\(.)", r"\1", manline)
+    return manline.replace('"', '').replace('\'', '')
+
+
+def get_synopsis(man):
+    try:
+        return " ".join(line for line in man[man.index("SYNOPSIS") + 1: man.index("DESCRIPTION")])
+    except ValueError:
+        return []
+
+
+def get_flags(man):
+    return [x for x in man if x[0] == '-']
 
 
 @sudo_support
