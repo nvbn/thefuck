4a4115f84939b8a4825d1508eb5643912192d5aa
Yotam Salmon <yotam.salmon@gmail.com>
2019-10-12 16:25:31 +0300

    Rule documentation

--- thefuck/rules/wrong_hyphen_before_subcommand.py
+++ thefuck/rules/wrong_hyphen_before_subcommand.py
@@ -1,30 +1,46 @@
-from thefuck.utils import get_all_executables, get_close_matches, \
-    get_valid_history_without_current, get_closest, which
+from thefuck.utils import get_all_executables
 from thefuck.specific.sudo import sudo_support
-from thefuck import logs
 
 
-import os.path
-import subprocess
-import gzip
 import re
+import gzip
+import os.path
 
 
 def get_manpage_dir():
+    """Returns the manpage directory for the system.
+
+    :rtype: str
+
+    Currently returns the default directory. On systems on which the manpage directory is configured
+    otherwise, this rule will not function. TODO: Dynamically find the directory.
+    """
     return "/usr/share/man"
 
 
-def get_manpage_path(command, number = None):
-    if number == None:
+def get_manpage_path(command, number=None):
+    """Finds the specific manpage path inside the manpages directory for a given command.
+
+    :type command: str
+    :type number: int (optional, if not provided the number will be auto detected)
+    :rtype: str (the path of the manpage for the command)
+    """
+    if number is None:
         return get_manpage_path(command, 1) or\
             get_manpage_path(command, 8) or\
             get_manpage_path(command, 7)
-    
+
     path = os.path.join(get_manpage_dir(), "man%d" % (number,), "%s.%d.gz" % (command, number))
     return path if os.path.exists(path) else None
 
 
-def get_manpage(command, number = None):
+def get_manpage(command, number=None):
+    """Returns the manpage for a command - a list of parsed lines
+
+    :type command: str
+    :type number: int (optional)
+    :rtype: [str]
+    """
     path = get_manpage_path(command, number)
     if path:
         with gzip.open(path, "r") as f:
@@ -34,20 +50,40 @@ def get_manpage(command, number = None):
 
 
 def textify(manline):
-    manline = re.sub(r"^\.[A-Za-z]+( |$)", "", str(manline, "utf-8").strip()) # Stripping line headers .SH, .B
-    manline = re.sub(r"\\f.", "", manline) # Stripping format
+    """Stringifies a manpage line (strips markdown instructions, unescapes characters and removes quotes)
+
+    :type manline: bytes
+    :rtype: str
+    """
+    # Stripping line headers .SH, .B
+    manline = re.sub(r"^\.[A-Za-z]+( |$)", "", str(manline, "utf-8").strip())
+
+    # Stripping format
+    manline = re.sub(r"\\f.", "", manline)
     manline = re.sub(r"\\(.)", r"\1", manline)
+
+    # Stripping quotes
     return manline.replace('"', '').replace('\'', '')
 
 
 def get_synopsis(man):
+    """Given a parsed manpage entry (as returned from get_manpage), returns the synopsis line from the manpage
+
+    :type man: [str]
+    :rtype: str
+    """
     try:
         return " ".join(line for line in man[man.index("SYNOPSIS") + 1: man.index("DESCRIPTION")])
     except ValueError:
-        return []
+        return ""
 
 
 def get_flags(man):
+    """Given a parsed manpage entry (as returned from get_manpage), returns a list of lines which contain flag information
+
+    :type man: [str]
+    :rtype: [str]
+    """
     return [x for x in man if x[0] == '-']
 
 
@@ -75,16 +111,19 @@ def match(command):
 
     return True
 
+
 @sudo_support
 def get_new_command(command):
     cmd, subcmd, *_ = command.script_parts[0].split('-')
     manpage = get_manpage(cmd)
     synopsis = get_synopsis(manpage)
 
-    if subcmd in synopsis: # We are dealing with an accidentally added hyphen
+    if subcmd in synopsis:
+        # We are dealing with an accidentally added hyphen
         return command.script.replace('-', ' ', 1)
 
-    else: # We are dealing with a missing space
+    else:
+        # We are dealing with a missing space
         return command.script.replace('-', ' -', 1)
 
 
