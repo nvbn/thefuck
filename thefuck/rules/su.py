f82ba5ff21b8a17494f7f8f71f7b7b323ec48497
Brandon Moore <moore.3071@osu.edu>
2019-11-10 21:34:31 -0500

    Added tests for su rule

--- thefuck/rules/su.py
+++ thefuck/rules/su.py
@@ -6,10 +6,7 @@ def match(command):
 
 
 def get_new_command(command):
-    if '&&' in command.script:
-        return u'su -c "sh -c "{}""'.format(" ".join([part for part in command.script_parts if part != "sudo"]))
-    elif '>' in command.script:
-        return u'su -c "sh -c "{}""'.format(command.script.replace('"', '\\"'))
-    else:
-        return u'su -c {}'.format(command.script)
+    return u'su -c "{}"'.format(command.script.replace('"', '\\"').replace('sudo ', ''))
+
+
 priority = 1200
