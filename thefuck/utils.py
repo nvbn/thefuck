77992029b6832992a8b2137e1489edd9705aeaa1
Tim Gates <tim.gates@iress.com>
2019-12-17 08:10:45 +1100

    Fix simple typo: controle -> control (#1017)

--- thefuck/utils.py
+++ thefuck/utils.py
@@ -98,7 +98,7 @@ def get_closest(word, possibilities, cutoff=0.6, fallback_to_first=True):
 
 
 def get_close_matches(word, possibilities, n=None, cutoff=0.6):
-    """Overrides `difflib.get_close_match` to controle argument `n`."""
+    """Overrides `difflib.get_close_match` to control argument `n`."""
     if n is None:
         n = settings.num_close_matches
     return difflib_get_close_matches(word, possibilities, n, cutoff)
