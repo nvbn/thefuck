b3c4b0c3c8048341996ea981ed0f3341148cfadf
Connor Martin <connormartin7@gmail.com>
2020-02-24 10:43:09 -0600

    add the rule

---
+++ thefuck/rules/git_lfs_mistype.py
@@ -0,0 +1,17 @@
+from thefuck.shells import shell
+from thefuck.specific.git import git_support
+
+@git_support
+def match(command):
+    '''
+    Match a mistyped command
+    '''
+    return 'lfs' in command.script and 'Did you mean this?' in command.output
+
+
+@git_support
+def get_new_command(command):
+    new = command.script.split(' ')
+    recommended = command.output.split('\n')[4].strip()
+    new[2] = recommended
+    return shell.and_(' '.join(new))
