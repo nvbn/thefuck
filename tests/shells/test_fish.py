565a6760e29aa579d3168068aa6e71b2880ca1a7
David <davidhart82@gmail.com>
2020-03-24 14:38:17 +0000

    Fix flaky test

--- tests/shells/test_fish.py
+++ tests/shells/test_fish.py
@@ -81,7 +81,8 @@ class TestFish(object):
         assert 'function FUCK' in shell.app_alias('FUCK')
         assert 'thefuck' in shell.app_alias('fuck')
         assert 'TF_SHELL=fish' in shell.app_alias('fuck')
-        assert 'TF_ALIAS=fuck TF_STATUS=$status PYTHONIOENCODING' in shell.app_alias('fuck')
+        assert 'TF_STATUS=$status' in shell.app_alias('fuck')
+        assert 'TF_ALIAS=fuck PYTHONIOENCODING' in shell.app_alias('fuck')
         assert 'PYTHONIOENCODING=utf-8 thefuck' in shell.app_alias('fuck')
         assert ARGUMENT_PLACEHOLDER in shell.app_alias('fuck')
 
