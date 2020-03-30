fa4f0921ea63584572d1ce688a4d73438471c2d6
Connor Martin <connormartin7@gmail.com>
2020-03-30 10:09:39 -0500

    get rid of additional matched strings

--- thefuck/rules/git_lfs_mistype.py
+++ thefuck/rules/git_lfs_mistype.py
@@ -14,5 +14,5 @@ def match(command):
 @git_support
 def get_new_command(command):
     broken_cmd = re.findall(r'Error: unknown command "([^"]*)" for "git-lfs"', command.output)[0]
-    matched = get_all_matched_commands(command.output, ['Did you mean'])
+    matched = get_all_matched_commands(command.output, ['Did you mean', ' for usage.'])
     return replace_command(command, broken_cmd, matched)
