a4cb9f6909bf274c66df7b4a2b12c498d0e82d78
ronan doolan <vagrant@vagrant.vm>
2020-04-28 14:11:51 +0000

    added gcloud cli

---
+++ thefuck/rules/gcloud_cli.py
@@ -0,0 +1,17 @@
+import re
+
+from thefuck.utils import for_app, replace_argument
+
+INVALID_CHOICE = "(?<=Invalid choice: ')(.*)(?=', maybe you meant:)"
+OPTIONS = "^\\s*\\*\\s(.*)"
+
+
+@for_app('gcloud')
+def match(command):
+    return "usage:" in command.output and "maybe you meant:" in command.output
+
+
+def get_new_command(command):
+    mistake = re.search(INVALID_CHOICE, command.output).group(0)
+    options = re.findall(OPTIONS, command.output, flags=re.MULTILINE)
+    return [replace_argument(command.script, mistake, o) for o in options]
