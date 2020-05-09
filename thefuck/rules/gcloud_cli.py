e304a370cd052cf101dd2e6dc72c39812e0f9ff0
Ronan Doolan <ronan.doolan2@mail.dcu.ie>
2020-05-09 16:15:41 +0100

    Remove duplicate blank lines again

--- thefuck/rules/gcloud_cli.py
+++ thefuck/rules/gcloud_cli.py
@@ -5,11 +5,9 @@ from thefuck.utils import for_app, replace_argument
 INVALID_CHOICE = "(?<=Invalid choice: ')(.*)(?=', maybe you meant:)"
 OPTIONS = "^\\s*\\*\\s(.*)"
 
-
 @for_app('gcloud')
 def match(command):
     return "usage:" in command.output and "maybe you meant:" in command.output
-
 
 def get_new_command(command):
     mistake = re.search(INVALID_CHOICE, command.output).group(0)
