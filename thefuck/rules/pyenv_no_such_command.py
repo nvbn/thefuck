8be4ae4cda510f6f7324ac46ca1c3d795b963508
gkodosis <geo_kont@live.com>
2020-07-03 15:34:31 +0300

    Test error source

--- thefuck/rules/pyenv_no_such_command.py
+++ thefuck/rules/pyenv_no_such_command.py
@@ -1,6 +1,7 @@
 import re
 from thefuck.utils import cache, for_app, replace_argument, replace_command, which
-from thefuck.specific.devenv import env_available, COMMON_TYPOS, get_commands
+from thefuck.specific.devenv import env_available, COMMON_TYPOS
+from subprocess import PIPE, Popen
 
 enabled_by_default = env_available
 
@@ -8,6 +9,14 @@ enabled_by_default = env_available
 @for_app('pyenv')
 def match(command):
     return 'pyenv: no such command' in command.output
+
+
+def get_commands(command):
+    if 'env' in command.script_parts[0]:
+        proc = Popen(['pyenv', 'commands'], stdout=PIPE)
+    else:
+        proc = Popen(['pyenv', 'commands'], stdout=PIPE)
+    return [line.decode('utf-8').strip() for line in proc.stdout.readlines()]
 
 
 if which('pyenv'):
