3505680869f3796b5e4fab0f04e92282f0da579a
gkodosis <geo_kont@live.com>
2020-07-03 16:35:43 +0300

    Revert test-source changes

--- thefuck/rules/pyenv_no_such_command.py
+++ thefuck/rules/pyenv_no_such_command.py
@@ -1,7 +1,6 @@
 import re
 from thefuck.utils import cache, for_app, replace_argument, replace_command, which
-from thefuck.specific.devenv import env_available, COMMON_TYPOS
-from subprocess import PIPE, Popen
+from thefuck.specific.devenv import env_available, COMMON_TYPOS, get_commands
 
 enabled_by_default = env_available
 
@@ -9,11 +8,6 @@ enabled_by_default = env_available
 @for_app('pyenv')
 def match(command):
     return 'pyenv: no such command' in command.output
-
-
-def get_commands():
-    proc = Popen(['pyenv', 'commands'], stdout=PIPE)
-    return [line.decode('utf-8').strip() for line in proc.stdout.readlines()]
 
 
 if which('pyenv'):
@@ -25,5 +19,5 @@ def get_new_command(command):
     broken = re.findall(r"pyenv: no such command `([^']*)'", command.output)[0]
     matched = [replace_argument(command.script, broken, common_typo)
                for common_typo in COMMON_TYPOS.get(broken, [])]
-    matched.extend(replace_command(command, broken, get_commands()))
+    matched.extend(replace_command(command, broken, get_commands(command)))
     return matched
