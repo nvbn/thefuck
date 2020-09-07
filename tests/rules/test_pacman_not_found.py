09ea18d35516b4a60b4e08024baa68f929931941
theslimshaney <33791263+theslimshaney@users.noreply.github.com>
2020-09-07 14:56:23 -0700

    Fix pacman tests

--- tests/rules/test_pacman_not_found.py
+++ tests/rules/test_pacman_not_found.py
@@ -4,8 +4,7 @@ from thefuck.rules import pacman_not_found
 from thefuck.rules.pacman_not_found import match, get_new_command
 from thefuck.types import Command
 
-PKGFILE_OUTPUT_LLC = '''extra/llvm 3.6.0-5      /usr/bin/llc
-extra/llvm35 3.5.2-13/usr/bin/llc'''
+PKGFILE_OUTPUT_LLC = 'extra/llvm 3.6.0-5      /usr/bin/llc'
 
 
 @pytest.mark.skipif(not getattr(pacman_not_found, 'enabled_by_default', True),
@@ -33,19 +32,19 @@ def test_match_mocked(subp_mock, command):
 @pytest.mark.skipif(not getattr(pacman_not_found, 'enabled_by_default', True),
                     reason='Skip if pacman is not available')
 @pytest.mark.parametrize('command, fixed', [
-    (Command('yay -S llc', 'error: target not found: llc'), ['yay -S extra/llvm', 'yay -S extra/llvm35']),
-    (Command('yaourt -S llc', 'error: target not found: llc'), ['yaourt -S extra/llvm', 'yaourt -S extra/llvm35']),
-    (Command('pacman -S llc', 'error: target not found: llc'), ['pacman -S extra/llvm', 'pacman -S extra/llvm35']),
-    (Command('sudo pacman -S llc', 'error: target not found: llc'), ['sudo pacman -S extra/llvm', 'sudo pacman -S extra/llvm35'])])
+    (Command('yay -S llc', 'error: target not found: llc'), ['yay -S extra/llvm']),
+    (Command('yaourt -S llc', 'error: target not found: llc'), ['yaourt -S extra/llvm']),
+    (Command('pacman -S llc', 'error: target not found: llc'), ['pacman -S extra/llvm']),
+    (Command('sudo pacman -S llc', 'error: target not found: llc'), ['sudo pacman -S extra/llvm'])])
 def test_get_new_command(command, fixed):
     assert get_new_command(command) == fixed
 
 
 @pytest.mark.parametrize('command, fixed', [
-    (Command('yay -S llc', 'error: target not found: llc'), ['yay -S extra/llvm', 'yay -S extra/llvm35']),
-    (Command('yaourt -S llc', 'error: target not found: llc'), ['yaourt -S extra/llvm', 'yaourt -S extra/llvm35']),
-    (Command('pacman -S llc', 'error: target not found: llc'), ['pacman -S extra/llvm', 'pacman -S extra/llvm35']),
-    (Command('sudo pacman -S llc', 'error: target not found: llc'), ['sudo pacman -S extra/llvm', 'sudo pacman -S extra/llvm35'])])
+    (Command('yay -S llc', 'error: target not found: llc'), ['yay -S extra/llvm']),
+    (Command('yaourt -S llc', 'error: target not found: llc'), ['yaourt -S extra/llvm']),
+    (Command('pacman -S llc', 'error: target not found: llc'), ['pacman -S extra/llvm']),
+    (Command('sudo pacman -S llc', 'error: target not found: llc'), ['sudo pacman -S extra/llvm'])])
 @patch('thefuck.specific.archlinux.subprocess')
 def test_get_new_command_mocked(subp_mock, command, fixed):
     subp_mock.check_output.return_value = PKGFILE_OUTPUT_LLC
