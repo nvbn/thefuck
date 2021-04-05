5084ba197386b72b21ef33ecd470fe87eb2136f4
Ryan Callahan <RyanCallahan312@gmail.com>
2021-04-05 15:20:45 -0400

    Revert "added confirmation function in logs.py"
    This reverts commit 488a33d1b785dc49895e5af493a3002a040f0af9.
    
    accidental commit to master

--- thefuck/entrypoints/fix_command.py
+++ thefuck/entrypoints/fix_command.py
@@ -6,17 +6,17 @@ from .. import logs, types, const
 from ..conf import settings
 from ..corrector import get_corrected_commands
 from ..exceptions import EmptyCommand
-from ..ui import select_command, confirm_command
+from ..ui import select_command
 from ..utils import get_alias, get_all_executables
 
 
 def _get_raw_command(known_args):
     if known_args.force_command:
         return known_args.force_command
-    elif not os.environ.get("TF_HISTORY"):
+    elif not os.environ.get('TF_HISTORY'):
         return known_args.command
     else:
-        history = os.environ["TF_HISTORY"].split("\n")[::-1]
+        history = os.environ['TF_HISTORY'].split('\n')[::-1]
         alias = get_alias()
         executables = get_all_executables()
         for command in history:
@@ -29,24 +29,20 @@ def _get_raw_command(known_args):
 def fix_command(known_args):
     """Fixes previous command. Used when `thefuck` called without arguments."""
     settings.init(known_args)
-    with logs.debug_time("Total"):
-        logs.debug(u"Run with settings: {}".format(pformat(settings)))
+    with logs.debug_time('Total'):
+        logs.debug(u'Run with settings: {}'.format(pformat(settings)))
         raw_command = _get_raw_command(known_args)
 
         try:
             command = types.Command.from_raw_script(raw_command)
         except EmptyCommand:
-            logs.debug("Empty command, nothing to do")
+            logs.debug('Empty command, nothing to do')
             return
 
         corrected_commands = get_corrected_commands(command)
         selected_command = select_command(corrected_commands)
 
-        confirmation = True
-        if selected_command.script == "reboot":
-            confirmation = confirm_command("Reboot System?")
-
-        if selected_command and confirmation:
+        if selected_command:
             selected_command.run(command)
         else:
             sys.exit(1)
