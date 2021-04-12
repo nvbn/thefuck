1f680de44cb2b019bf88f5fa4ac870a707bb8051
Ryan Callahan <RyanCallahan312@gmail.com>
2021-04-12 18:03:32 -0400

    Add rvm use support
    gets the error output from rvm use when run in a subprocess spawned by python.
    
    fixes the command by first installing the request ruby version then running rvm use.

---
+++ thefuck/rules/rvm_use.py
@@ -0,0 +1,18 @@
+from thefuck.utils import for_app
+
+
+@for_app('rvm', at_least=2)
+def match(command):
+    args = command.script_parts
+    pattern = f"""RVM is not a function, selecting rubies with 'rvm use ...' will not work.
+
+You need to change your terminal emulator preferences to allow login shell.
+Sometimes it is required to use `/bin/bash --login` as the command.
+Please visit https://rvm.io/integration/gnome-terminal/ for an example."""
+
+    return args[1] == 'use' and pattern in command.output
+
+
+def get_new_command(command):
+    args = command.script_parts
+    return f'rvm install \"ruby-{args[2]}\"; rvm use {args[2]}'
