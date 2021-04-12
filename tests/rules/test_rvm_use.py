6d9825cc02b54887feaed0a01315f6f2bde3e62a
Ryan Callahan <RyanCallahan312@gmail.com>
2021-04-12 18:42:54 -0400

    remove another fstring
    remove another fstring that I forgot :smile:

--- tests/rules/test_rvm_use.py
+++ tests/rules/test_rvm_use.py
@@ -3,7 +3,7 @@ from thefuck.rules.rvm_use import match, get_new_command
 from thefuck.types import Command
 
 
-output = pattern = f"""RVM is not a function, selecting rubies with 'rvm use ...' will not work.
+output = pattern = """RVM is not a function, selecting rubies with 'rvm use ...' will not work.
 
 You need to change your terminal emulator preferences to allow login shell.
 Sometimes it is required to use `/bin/bash --login` as the command.
