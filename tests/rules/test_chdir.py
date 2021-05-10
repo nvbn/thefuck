6931d37b5ee151f8ec9ac5cc7460255a42610e1d
ian <icalhoun@student.bridgew.edu>
2021-05-10 11:42:13 -0400

    modified test_cd_mkdir.py tests so that they work with the modified cd_mkdir.py. Modified test_cd_mkdir.py to work with the chdir.py rule as well and saved it as test_chdir.py. I also added a test for invalid directory names to both test files.

--- tests/rules/test_chdir.py
+++ tests/rules/test_chdir.py
@@ -1,12 +1,12 @@
 import pytest
-from thefuck.rules.cd_mkdir import match, get_new_command
+from thefuck.rules.chdir import match, get_new_command
 from thefuck.types import Command
 
 
 @pytest.mark.parametrize('command', [
     Command('chdir foo', 'cannot find the path'),
     Command('chdir foo/bar', 'can\'t cd to'),
-    Command('chdir foo/bar', 'cannot find paht'),
+    Command('chdir foo/bar', 'cannot find path'),
     Command('chdir /foo/bar/', 'can\'t cd to')])
 def test_match(command):
     assert match(command)
@@ -19,7 +19,13 @@ def test_not_match(command):
 
 
 @pytest.mark.parametrize('command, new_command', [
-    (Command('chdir foo', ''), 'mkdir -p foo && chdir foo'),
-    (Command('chdir foo/bar', ''), 'mkdir -p foo/bar && chdir foo/bar')])
+    (Command('chdirfoo', ''), 'mkdir -p foo; chdir foo'),
+    (Command('chdirfoo/bar/baz', ''), 'mkdir -p foo/bar/baz; chdir foo/bar/baz')])
 def test_get_new_command(command, new_command):
     assert get_new_command(command) == new_command
+
+
+@pytest.mark.parametrize('command', [
+    Command('chdir <', ''), Command('chdir \0', '')])
+def test_bad_dir_name(command):
+    assert not match(command)
