11b70526f72339bcce0beefa1665c74bb06e18b3
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-07-08 21:43:35 +0200

    #1131: Improve git_commit_add rule
    Add more capabilities to the rule, remove its priority and fix tests

--- tests/rules/test_git_commit_add.py
+++ tests/rules/test_git_commit_add.py
@@ -3,34 +3,36 @@ from thefuck.rules.git_commit_add import match, get_new_command
 from thefuck.types import Command
 
 
-@pytest.mark.parametrize('script, output', [
-    ('git commit -m "test"', 'no changes added to commit'),
-    ('git commit', 'no changes added to commit')])
+@pytest.mark.parametrize(
+    "script, output",
+    [
+        ('git commit -m "test"', "no changes added to commit"),
+        ("git commit", "no changes added to commit"),
+    ],
+)
 def test_match(output, script):
     assert match(Command(script, output))
 
 
-@pytest.mark.parametrize('script, output', [
-    ('git commit -m "test"', ' 1 file changed, 15 insertions(+), 14 deletions(-)')])
+@pytest.mark.parametrize(
+    "script, output",
+    [
+        ('git commit -m "test"', " 1 file changed, 15 insertions(+), 14 deletions(-)"),
+        ("git branch foo", ""),
+        ("git checkout feature/test_commit", ""),
+        ("git push", ""),
+    ],
+)
 def test_not_match(output, script):
     assert not match(Command(script, output))
 
 
-@pytest.mark.parametrize('script', [
-    'git branch foo',
-    'git checkout feature/test_commit',
-    'git push'])
-def test_not_match_either(script):
-    assert not match(Command(script, ''))
-
-
-@pytest.mark.parametrize('script', [
-    ('git commit')])
-def test_get_new_command_one(script):
-    assert get_new_command(Command(script, '')) == 'git commit -a'
-
-
-@pytest.mark.parametrize('script', [
-    ('git commit -m "test commit"')])
-def test_get_new_command_two(script):
-    assert get_new_command(Command(script, '')) == 'git commit -a -m "test commit"'
+@pytest.mark.parametrize(
+    "script, new_command",
+    [
+        ("git commit", ["git commit -a", "git commit -p"]),
+        ('git commit -m "foo"', ['git commit -a -m "foo"', 'git commit -p -m "foo"']),
+    ],
+)
+def test_get_new_command(script, new_command):
+    assert get_new_command(Command(script, "")) == new_command
--- thefuck/rules/git_commit_add.py
+++ thefuck/rules/git_commit_add.py
@@ -1,15 +1,17 @@
-from thefuck.utils import replace_argument
+from thefuck.utils import eager, replace_argument
 from thefuck.specific.git import git_support
-
-priority = 900  # Lower first, default is 1000
 
 
 @git_support
 def match(command):
-    return ('commit' in command.script_parts
-            and 'no changes added to commit' in command.output)
+    return (
+        "commit" in command.script_parts
+        and "no changes added to commit" in command.output
+    )
 
 
+@eager
 @git_support
 def get_new_command(command):
-    return replace_argument(command.script, 'commit', 'commit -a')
+    for opt in ("-a", "-p"):
+        yield replace_argument(command.script, "commit", "commit {}".format(opt))
