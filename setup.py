3f71959b1ba9e8359cc0e42226706a742e82ab8b
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-11-23 16:07:27 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(name='thefuck',
                                       'tests', 'tests.*', 'release']),
       include_package_data=True,
       zip_safe=False,
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
       install_requires=install_requires,
       extras_require=extras_require,
       entry_points={'console_scripts': [
