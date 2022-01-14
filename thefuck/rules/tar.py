347a298739e0473fa0f716ac33167a93166f8c54
yaacornus <96870156+yaacornus@users.noreply.github.com>
2022-01-14 20:42:58 +0800

    add more options and improved filter

--- thefuck/rules/tar.py
+++ thefuck/rules/tar.py
@@ -6,11 +6,12 @@ patterns = ['tar :', 'exiting now',
             'tar: option requires an argument',
             'option requires an argument',
             'Try tar \'--help\' or \'tar --usage\' for more information',
-            'try \'tar --help\'', 'try \'tar usage\'']
+            'try \'tar --help\'', 'try \'tar usage\'',
+            'tar: Refusing to read archive contents from terminal (missing -f option?)',]
 
 options = ['-cf', '-tvf', '-xf', 'cvf', 'cvzf',
            'cvzfj', '-xvf', '-tvf', '-zvxf', '-jvxf',
-           '-zxvf', '-rvf']
+           '-zxvf', '-rvf', '-czf', 'tvfW', '-rvf', '-jxvf']
 
 def match(command):
     
@@ -20,12 +21,21 @@ def match(command):
     return False    
 
 def get_new_command(command):
-    
+
+    # isolate sudo, usually the layout is tar [option] [files -> [input] [output]]     
+    # for len                               1      2                 3       4
+    # for list                              0       1              2
     com_parts = [part for part in command.script_parts if part != 'sudo']
-    isolated = sorted([opt for part in command.script_parts for opt in options if SM(None, part.lower(), opt).ratio() > 0.5])
     
+    # see if the percent similarity of the option is greater than 0.5 (50%)
+    isolated = [opt for part in command.script_parts for opt in options if SM(None, part.lower(), opt).ratio() > 0.5]
+    
+    # having a length of more than 2 implies that it is likely that it has an input
+    # and output, rather than a trivial mistake
     if len(com_parts) > 2 :     
         return f"{com_parts[0]} {isolated[0]} {' '.join(com_parts[2:len(com_parts)])}"
     else:
-        return f"{com_parts[0]} {isolated[0]}"
-            
+        if isolated == []:
+            return f"{' '.join([parts for parts in com_parts])}"
+        else:
+            return f"{com_parts[0]} {isolated[0]}"        
