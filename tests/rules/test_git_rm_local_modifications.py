a0e6758604924a2da9ae9200df72d5c7ef33ce9b
Owen Jaques <owenjaques2000@gmail.com>
2022-03-17 15:40:17 -0700

    Succesfully refactored. All rules tests are passing.

--- tests/rules/test_git_rm_local_modifications.py
+++ tests/rules/test_git_rm_local_modifications.py
@@ -1,5 +1,5 @@
 import pytest
-from thefuck.rules.git_rm_local_modifications import match, get_new_command
+from thefuck.rules.git_rm_staged_and_local_modifications import match, get_new_command
 from thefuck.types import Command
 
 
