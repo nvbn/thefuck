a3c40fc05a180e7b9d02ae30280f1bd716fb6ac7
Miguel Guthridge <hdsq@outlook.com.au>
2022-05-24 15:11:51 +1000

    Clean up tests and improve matching

--- tests/rules/test_git_clone_missing.py
+++ tests/rules/test_git_clone_missing.py
@@ -1,37 +1,53 @@
-from thefuck.rules.git_clone_git_clone import match, get_new_command
+import pytest
+from thefuck.rules.git_clone_missing import match, get_new_command
 from thefuck.types import Command
 
-output_clean = """
-fatal: Too many arguments.
-
-usage: git clone [<options>] [--] <repo> [<dir>]
-"""
-
-http_url = 'https://github.com/nvbn/thefuck.git'
+https_url = 'https://github.com/nvbn/thefuck.git'
+http_url = 'http://github.com/nvbn/thefuck.git'
 ssh_url = 'git@github.com:nvbn/thefuck.git'
 
 # HELP WANTED: How can I make this independent of the shell?
+https_output = """
+bash: https://github.com/nvbn/thefuck.git: No such file or directory
+"""
 http_output = """
 bash: https://github.com/nvbn/thefuck.git: No such file or directory
 """
 ssh_output = """
-bash: hgit@github.com:nvbn/thefuck.git: No such file or directory
+bash: git@github.com:nvbn/thefuck.git: No such file or directory
 """
 
+@pytest.mark.parametrize(
+    'cmd', [
+        Command(https_url, https_output),
+        Command(http_url, http_output),
+        Command(ssh_url, ssh_output),
+    ]
+)
+def test_match(cmd):
+    assert match(cmd)
 
-def test_match():
-    assert match(Command(http_url, http_output))
-    assert match(Command(ssh_url, ssh_output))
+@pytest.mark.parametrize(
+    'cmd', [
+        Command('', ''),
+        Command('git branch', ''),
+        Command('git clone foo', ''),
+        Command('git clone foo bar baz', ''),
+        Command('git clone ' + http_url, http_output),
+        Command('git@example.com:thing.gut', ssh_output),
+        Command('https:/github.com/nvbn/thefuck.git', http_output),
+    ]
+)
+def test_not_match(cmd):
+    assert not match(cmd)
 
 
-def test_not_match():
-    assert not match(Command('', ''))
-    assert not match(Command('git branch', ''))
-    assert not match(Command('git clone foo', ''))
-    assert not match(Command('git clone foo bar baz', output_clean))
-    assert not match(Command('git clone ' + http_url, ''))
-
-
-def test_get_new_command():
-    assert get_new_command(Command(http_url, http_output)) == 'git clone ' + http_url
-    assert get_new_command(Command(ssh_url, ssh_output)) == 'git clone ' + ssh_url
+@pytest.mark.parametrize(
+    'cmd', [
+        Command(https_url, https_output),
+        Command(http_url, http_output),
+        Command(ssh_url, ssh_output),
+    ]
+)
+def test_get_new_command(cmd):
+    assert get_new_command(cmd) == 'git clone ' + cmd.script
--- thefuck/rules/git_clone_missing.py
+++ thefuck/rules/git_clone_missing.py
@@ -1,15 +1,19 @@
 from thefuck.specific.git import git_support
 
+output_contains = 'No such file or directory'
 # Note: could probably use better checking for URLs
 
-@git_support
+
+# @git_support
 def match(command):
+    if not output_contains in command.output:
+        return False
     return command.script.endswith('.git') and (
         (  # HTTP cloning
             command.script.startswith('https://')
             or command.script.startswith('http://')
         )
-        or (  # SSH cloning: user@website.com:path/to/repo.git
+        or (  # SSH cloning: user[@]website.com[:]path/to/repo[.git]
             command.script.find('@') < command.script.find(':')
             and command.script.find('@') != -1
             and command.script.find(':') != -1
@@ -17,6 +21,6 @@ def match(command):
     )
 
 
-@git_support
+# @git_support
 def get_new_command(command):
     return 'git clone ' + command.script
