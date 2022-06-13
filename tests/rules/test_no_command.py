116cf4c989df15bb0d9f93e2cfceb8c535e4d779
Pablo Aguiar <scorphus@gmail.com>
2022-06-13 17:59:41 +0200

    Update tests/rules/test_no_command.py

--- tests/rules/test_no_command.py
+++ tests/rules/test_no_command.py
@@ -22,7 +22,7 @@ def history_without_current(mocker):
     ('fucck', 'fucck: not found'),
     ('puthon', "'puthon' is not recognized as an internal or external command"),
     ('got commit', 'got: command not found'),
-    ('''gti commit -m "new commit" ''', 'gti: command not found')])
+    ('gti commit -m "new commit"', 'gti: command not found')])
 def test_match(mocker, script, output):
     mocker.patch('thefuck.rules.no_command.which', return_value=None)
 
@@ -45,6 +45,6 @@ def test_not_match(mocker, script, output, which):
     ('vom file.py', ['vim file.py']),
     ('fucck', ['fsck']),
     ('got commit', ['git commit', 'go commit']),
-    ("gti commit -m 'new commit'", ["git commit -m 'new commit'"])])
+    ('gti commit -m "new commit"', ['git commit -m "new commit"'])])
 def test_get_new_command(script, result):
     assert get_new_command(Command(script, '')) == result
