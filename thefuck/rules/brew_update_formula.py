4fccbdc0afe458356752b37d13415192b4dca9b9
pyrrolizin <piti.x@gmx.net>
2022-06-23 17:11:52 +0200

    fix: brew_update_formula change output string

--- thefuck/rules/brew_update_formula.py
+++ thefuck/rules/brew_update_formula.py
@@ -1,11 +1,14 @@
+import re
 from thefuck.utils import for_app
+
+message_regex = re.compile(r"Use ['\`]brew upgrade")
 
 
 @for_app('brew', at_least=2)
 def match(command):
     return ('update' in command.script
             and "Error: This command updates brew itself" in command.output
-            and "Use 'brew upgrade" in command.output)
+            and message_regex.search(command.output))
 
 
 def get_new_command(command):
