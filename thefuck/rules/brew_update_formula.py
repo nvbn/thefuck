ccafc39d1e07766a65f3de903bd87b684a75667c
pyrrolizin <piti.x@gmx.net>
2022-06-27 10:42:48 +0200

    fix: regex removed, test: backtick added

--- thefuck/rules/brew_update_formula.py
+++ thefuck/rules/brew_update_formula.py
@@ -1,14 +1,11 @@
-import re
 from thefuck.utils import for_app
-
-message_regex = re.compile(r"Use ['\`]brew upgrade")
 
 
 @for_app('brew', at_least=2)
 def match(command):
     return ('update' in command.script
             and "Error: This command updates brew itself" in command.output
-            and message_regex.search(command.output))
+            and r"Use `brew upgrade" in command.output)
 
 
 def get_new_command(command):
