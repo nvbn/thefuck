0f3c19c25bc6ee30e4ba6979db58a17317f7a81b
pyrrolizin <piti.x@gmx.net>
2022-06-29 14:08:08 +0200

    fix: Update output for brew_install test: fixed

--- tests/rules/test_brew_install.py
+++ tests/rules/test_brew_install.py
@@ -1,17 +1,16 @@
 import pytest
 from thefuck.rules.brew_install import match, get_new_command
-from thefuck.rules.brew_install import _get_formulas
 from thefuck.types import Command
 
 
 @pytest.fixture
 def brew_no_available_formula():
-    return '''Error: No available formula for elsticsearch '''
+    return '''Warning: No available formula with the name "gitt". Did you mean git, gitg or gist?'''
 
 
 @pytest.fixture
 def brew_install_no_argument():
-    return '''This command requires a formula argument'''
+    return '''Install a formula or cask. Additional options specific to a formula may be'''
 
 
 @pytest.fixture
@@ -19,27 +18,21 @@ def brew_already_installed():
     return '''Warning: git-2.3.5 already installed'''
 
 
-def _is_not_okay_to_test():
-    return 'elasticsearch' not in _get_formulas()
 
 
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
 def test_match(brew_no_available_formula, brew_already_installed,
                brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
+    assert match(Command('brew install gitt',
                          brew_no_available_formula))
     assert not match(Command('brew install git',
                              brew_already_installed))
     assert not match(Command('brew install', brew_install_no_argument))
 
 
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
 def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
+    assert get_new_command(Command('brew install gitt',
                                    brew_no_available_formula))\
-        == 'brew install elasticsearch'
+        == 'brew install git'
 
     assert get_new_command(Command('brew install aa',
                                    brew_no_available_formula))\
