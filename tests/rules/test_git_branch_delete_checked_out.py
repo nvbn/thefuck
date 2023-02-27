4908aaa423e8859032e87aa2f862f5685842cc57
KerH <keresh3@gmail.com>
2023-02-27 11:39:24 +0000

    checkout to default branch instead of master

--- tests/rules/test_git_branch_delete_checked_out.py
+++ tests/rules/test_git_branch_delete_checked_out.py
@@ -1,4 +1,7 @@
+from mock import patch
+
 import pytest
+
 from thefuck.rules.git_branch_delete_checked_out import match, get_new_command
 from thefuck.types import Command
 
@@ -25,5 +28,18 @@ def test_not_match(script):
         ("git branch -D foo", "git checkout master && git branch -D foo"),
     ],
 )
-def test_get_new_command(script, new_command, output):
-    assert get_new_command(Command(script, output)) == new_command
+def test_get_new_command_deletion_flag(script, new_command, output):
+    with patch('thefuck.rules.git_branch_delete_checked_out.get_sp_stdout', return_value='master'):
+        assert get_new_command(Command(script, output)) == new_command
+
+
+@pytest.mark.parametrize(
+    "script, default_branch",
+    [
+        ("git branch -d foo", "main"),
+        ("git branch -d foo", "bar"),
+    ],
+)
+def test_get_new_command_default_branch(script, default_branch, output):
+    with patch('thefuck.rules.git_branch_delete_checked_out.get_sp_stdout', return_value=default_branch):
+        assert get_new_command(Command(script, output)) == "git checkout {default_branch} && git branch -D foo".format(default_branch=default_branch)
