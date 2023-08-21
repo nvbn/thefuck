c469919246320db749c2242b64afd121e9d32d78
DashBing <mcbbkf@outlook.com>
2023-08-21 11:26:34 +0800

    fix all tests/entrypoints/*

--- tests/entrypoints/test_alias.py
+++ tests/entrypoints/test_alias.py
@@ -4,23 +4,29 @@ from thefuck.entrypoints.alias import _get_alias, print_alias
 
 
 @pytest.mark.parametrize(
-    'py2, enable_experimental_instant_mode, which, is_instant', [
+    'py2, enable_experimental_instant_mode, which, is_instant',
+    [
         (False, True, True, True),
         (False, False, True, False),
         (False, True, False, False),
         (True, True, True, False),
         (True, True, False, False),
-        (True, False, True, False)])
+        (True, False, True, False)
+    ]
+)
 def test_get_alias(monkeypatch, mocker, py2,
                    enable_experimental_instant_mode,
                    which, is_instant):
     monkeypatch.setattr('six.PY2', py2)
     args = Mock(
         enable_experimental_instant_mode=enable_experimental_instant_mode,
-        alias='fuck', )
+        alias='fuck',
+    )
     mocker.patch('thefuck.entrypoints.alias.which', return_value=which)
-    shell = Mock(app_alias=lambda _: 'app_alias',
-                 instant_mode_alias=lambda _: 'instant_mode_alias')
+    shell = Mock(
+        app_alias=lambda _: 'app_alias',
+        instant_mode_alias=lambda _: 'instant_mode_alias'
+    )
     monkeypatch.setattr('thefuck.entrypoints.alias.shell', shell)
 
     alias = _get_alias(args)
