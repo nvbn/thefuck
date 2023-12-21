8ce1b4d380fab9f34b5da15a7692070f7b0c721f
Ruslan Semagin <pixel.365.24@gmail.com>
2023-12-21 10:23:09 +0300

    Update list of 'Brew' commands

--- tests/rules/test_brew_unknown_command.py
+++ tests/rules/test_brew_unknown_command.py
@@ -14,16 +14,36 @@ def brew_unknown_cmd2():
     return '''Error: Unknown command: instaa'''
 
 
+@pytest.fixture
+def brew_unknown_cmd_auto():
+    return '''Error: Unknown command: auto'''
+
+
+@pytest.fixture
+def brew_unknown_cmd_outdated():
+    return '''Error: Unknown command: outdate'''
+
+
 def test_match(brew_unknown_cmd):
     assert match(Command('brew inst', brew_unknown_cmd))
     for command in _brew_commands():
         assert not match(Command('brew ' + command, ''))
 
 
-def test_get_new_command(brew_unknown_cmd, brew_unknown_cmd2):
+def test_get_new_command(brew_unknown_cmd, brew_unknown_cmd2, brew_unknown_cmd_auto,
+                         brew_unknown_cmd_outdated):
     assert (get_new_command(Command('brew inst', brew_unknown_cmd))
             == ['brew list', 'brew install', 'brew uninstall'])
 
     cmds = get_new_command(Command('brew instaa', brew_unknown_cmd2))
     assert 'brew install' in cmds
     assert 'brew uninstall' in cmds
+
+    cmds = get_new_command(Command('brew auto', brew_unknown_cmd_auto))
+    assert 'brew audit' in cmds
+    assert 'brew autoremove' in cmds
+
+    cmds = get_new_command(Command('brew outdate', brew_unknown_cmd_outdated))
+    assert 'brew outdated' in cmds
+    assert 'brew update' in cmds
+    assert 'brew bottle' in cmds
